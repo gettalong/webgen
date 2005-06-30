6223e3ede998031fdb07adf5cc396f50aa7bb7c6
Thomas Leitner <t_leitner@gmx.at>
2005-06-30 07:30:17 +0000

    * added website for generating gallery layout pictures * moved webgen-gallery-creator to bin directory

---
+++ otherdata/web-for-gallery-pics/create_pictures.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'webgen/configuration'
+require 'RMagick'
+
+def capture_image
+  image = Magick::Image.capture( true, false, false, true ) { self.filename = 'root' }
+  image.crop!(0, 105, 1024, 636)
+  image.change_geometry( '200x150' ) {|c,r,i| i.resize!( c, r )}
+  image
+end
+
+Webgen::Plugin['Configuration'].init_all
+layouts = Webgen::Plugin.config[PictureGalleryLayouter::DefaultGalleryLayouter].layouts
+
+system('ruby -I../../lib ../../bin/webgen')
+system('opera -geometry 1024x768+0+0 -newpage `pwd`/output/ &')
+sleep( 10 )
+
+
+layouts.keys.each do |name|
+  system("opera `pwd`/output/Gallery_#{name}.html &")
+  sleep( 3 )
+  image1 = capture_image
+
+  system("opera `pwd`/output/Gallery_#{name}_2.html &")
+  sleep( 3 )
+  image2 = capture_image
+
+  system("opera `pwd`/output/Gallery_#{name}_webgen3_png.html &")
+  sleep( 3 )
+  image3 = capture_image
+
+  image = Magick::Image.new( 600, 150)
+  image.composite!( image1, 0, 0, Magick::CopyCompositeOp )
+  image.composite!( image2, 200, 0, Magick::CopyCompositeOp )
+  image.composite!( image3, 400, 0, Magick::CopyCompositeOp )
+  image.write( "../../data/webgen/gallery-creator/#{name}.png" )
+  puts "Wrote gallery image for layout #{name}"
+end
+
+system('killall opera')
---
+++ otherdata/web-for-gallery-pics/plugin/gallery.rb
@@ -0,0 +1,32 @@
+require 'yaml'
+require 'webgen/plugins/filehandler/filehandler'
+require 'webgen/plugins/filehandler/picturegallery'
+
+module FileHandlers
+
+  class GallerySampler < DefaultFileHandler
+
+    summary "Creates gallery samples"
+    extension 'sample'
+    depends_on 'FileHandler'
+
+
+    def create_node( path, parent )
+      layouts = Webgen::Plugin.config[PictureGalleryLayouter::DefaultGalleryLayouter].layouts
+      data = YAML::load( File.read( path ) )
+      layouts.keys.each do |name|
+        data['layout'] = name
+        data['title'] = "Gallery #{name}"
+        File.open( path, 'w+' ) {|file| file.write( data.to_yaml )}
+        Webgen::Plugin['PictureGalleryFileHandler'].create_node( path, parent )
+      end
+      nil
+    end
+
+    def write_node( node )
+      # nothing to write
+    end
+
+  end
+
+end
