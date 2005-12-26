5970a178267b3764afb93f367f74ccc0b73c628b
Thomas Leitner <t_leitner@gmx.at>
2005-12-26 17:43:53 +0000

    * created directory for text fixtures * moved unit tests in separate unittests directory

---
+++ test/unittests/util/tc_composite.rb
@@ -0,0 +1,77 @@
+require 'test/unit'
+require 'webgen/composite'
+
+
+class CompositeTest < Test::Unit::TestCase
+
+  class CompositeTestclass
+    include Composite
+  end
+
+
+  def setup
+    @testclass = CompositeTestclass.new
+    @children = ['hello', 'beta', 'gamma']
+  end
+
+
+  def test_initialize
+    assert_kind_of( Composite, @testclass )
+    assert_not_nil( @testclass.children )
+    assert_equal( 0, @testclass.children.length )
+  end
+
+
+  def test_add_children
+    assert_nothing_raised do
+      @testclass.add_children @children
+    end
+    assert_equal( @children, @testclass.children )
+    assert_raise( ArgumentError ) do
+      @testclass.add_children 'test'
+    end
+  end
+
+
+  def test_del_children
+    assert_nothing_raised do @testclass.del_children end
+    @testclass.add_children @children
+    assert_equal( @children, @testclass.children )
+    @testclass.del_children
+    assert_equal( 0, @testclass.children.length )
+  end
+
+
+  def test_add_child
+    child = "hello"
+    assert_nothing_raised do
+      @testclass.add_child child
+      assert_equal( 1, @testclass.children.length )
+      assert_equal( child, @testclass.children[0] )
+
+      @testclass.add_child child
+      assert_equal( 1, @testclass.children.length )
+    end
+  end
+
+
+  def test_del_child
+    assert_nothing_raised do
+      @testclass.add_children @children
+      @testclass.del_child 'beta'
+      assert_equal( ['hello', 'gamma'], @testclass.children )
+      @testclass.del_child 1
+      assert_equal( ['hello'], @testclass.children )
+    end
+  end
+
+
+  def test_has_children
+    assert_nothing_raised do
+      assert( !@testclass.has_children? )
+      @testclass.add_children @children
+      assert( @testclass.has_children? )
+    end
+  end
+
+end
