e3e663962cb0b194b7d72e316ddd0024509651a3
Thomas Leitner <t_leitner@gmx.at>
2006-07-04 08:17:03 +0000

    * moved class ExtendendCommand to its own file

--- lib/webgen/plugins/htmlvalidators/xmllint.rb
+++ lib/webgen/plugins/htmlvalidators/xmllint.rb
@@ -21,29 +21,7 @@
 #
 
 require 'webgen/plugins/htmlvalidators/default'
-require "tempfile"
-
-# Allows one to get stdout and stderr from an executed command. Original version
-# by Karl von Laudermann in ruby-talk #113035
-class ExtendedCommand
-
-  attr_reader :ret_code, :out_text, :err_text
-
-  def initialize( command )
-    tempfile = Tempfile.new( 'xmllint' )
-    tempfile.close  # So that child process can write to it
-
-    # Execute command, redirecting stderr to temp file
-    @out_text = `#{command} 2> #{tempfile.path}`
-    @ret_code = $? >> 8
-
-    # Read temp file
-    tempfile.open
-    @err_text = tempfile.readlines.join
-    tempfile.close
-  end
-end
-
+require 'webgen/extcommand'
 
 module HtmlValidators
 
