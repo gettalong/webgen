9b0b36388c9cfbd4d7620fab1a4b9cc41c692c68
Thomas Leitner <t_leitner@gmx.at>
2007-04-11 15:45:00 +0000

    * updated #param to work with new infrastructure and added FileConfigurator

--- lib/webgen/cli.rb
+++ lib/webgen/cli.rb
@@ -1,25 +1,3 @@
-#
-#--
-#
-# $Id: cli.rb 601 2007-02-14 21:20:44Z thomas $
-#
-# webgen: template based static website generator
-# Copyright (C) 2004 Thomas Leitner
-#
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with this program; if not,
-# write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-#
-#++
-#
-
 require 'rbconfig'
 require 'fileutils'
 require 'cmdparse'
@@ -50,16 +28,13 @@ module Webgen
       end
       self.add_command( CmdParse::HelpCommand.new )
       self.add_command( CmdParse::VersionCommand.new )
-
     end
 
-    def param_for_plugin( plugin_name, param )
-      if [plugin_name, param] == ['Core/Configuration', 'loggerLevel'] && @verbosity != VERBOSITY_UNUSED
-        @verbosity
-      elsif @config_file
-        @config_file.param_for_plugin( plugin_name, param )
+    def param( param, plugin, cur_val )
+      if [plugin, param] == ['Core/Configuration', 'loggerLevel'] && @verbosity != VERBOSITY_UNUSED
+        [true, @verbosity]
       else
-        Webgen::PluginParamValueNotFound
+        [false, cur_val]
       end
     end
 
@@ -69,6 +44,7 @@ module Webgen
           #@config_file = Webgen::WebSite.load_config_file( @directory )
           @website = Webgen::WebSite.new( @directory )
           @website.plugin_manager.configurators << self
+          @website.plugin_manager.configurators << FileConfigurator.new( File.join( @directory, 'config.yaml' ) ) #TODO: make creating Fileconfigurator easier (just with directory param)
           @website.load_plugin_infos
           @website.plugin_manager.plugin_infos[%r{^Cli/Commands/}].each do |name, info|
             self.add_command( @website.plugin_manager[name] )
