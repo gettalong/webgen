20f19d5bcb4a00689b5c5a58064e8628fd2f2984
Thomas Leitner <t_leitner@gmx.at>
2007-08-20 08:44:23 +0000

    * added basic Maruku content processor

---
+++ data/webgen/plugins/contentprocessor/maruku.plugin/maruku.rb
@@ -0,0 +1,16 @@
+require 'maruku'
+
+module ContentProcessor
+
+  # Handles content in Markdown+Extras format using Maruku.
+  class Maruku
+
+    def process( content, context, options )
+      Maruku.new( content ).to_html
+    rescue Exception => e
+      raise "Maruku to HTML conversion failed: #{e.message}"
+    end
+
+  end
+
+end
