535be188dcf24a0e696fbc6664d43ca7091d1f5b
Thomas Leitner <t_leitner@gmx.at>
2007-12-29 09:31:32 +0000

    * implemented T004: ContentProcessors/Blocks now accepts an optional attribute with which the node chain can be specified that should be used for inserting the rendered block

--- data/webgen/plugins/contentprocessor/blocks.plugin/blocks.rb
+++ data/webgen/plugins/contentprocessor/blocks.plugin/blocks.rb
@@ -2,17 +2,28 @@ module ContentProcessor
 
   class Blocks
 
-    BLOCK_RE = /<webgen:block\s+name=('|")(\w+)\1\s*\/>/
+    BLOCK_RE = /<webgen:block\s+name=('|")(\w+)\1\s*(?:\schain=('|")([^'"]+)\3\s*)?\s*\/>/
 
     def process( context )
       chain = context.chain
 
-      block_node = (chain.length > 1 ? chain[1] : chain[0])
-      new_chain = (chain[1..-1].empty? ? chain : chain[1..-1])
+      new_chain = (chain.length > 1 ? chain[1..-1] : chain)
       context.cache_info[plugin_name] ||= []
 
       context.content.gsub!( BLOCK_RE ) do |match|
         block_name = $2
+        if $4.nil?
+          used_chain = new_chain
+        else
+          paths = $4.split(';')
+          used_chain = paths.collect do |path|
+            temp_node = @plugin_manager['Core/CacheManager'].node_for_path( context.ref_node, path.strip, context.node['lang'] )
+            log(:error) { "Could not resolve <#{path.strip}> in <#{context.ref_node.absolute_lcn}> in '#{match.to_s}'" } if temp_node.nil?
+            temp_node
+          end.compact
+          next match if used_chain.length != paths.length
+        end
+        block_node = used_chain.first
 
         if !block_node.node_info[:page].blocks.has_key?( block_name )
           raise "Node <#{block_node.absolute_lcn}> has no block named '#{block_name}'"
@@ -20,7 +31,7 @@ module ContentProcessor
 
         log(:debug) { "Inserting rendered node <#{block_node.absolute_lcn}> into <#{chain.first.absolute_lcn}>" }
         context.cache_info[plugin_name] << block_node.absolute_lcn
-        tmp_context = block_node.node_info[:page].blocks[block_name].render( context.clone(:chain => new_chain) )
+        tmp_context = block_node.node_info[:page].blocks[block_name].render( context.clone(:chain => used_chain) )
         tmp_context.content
       end
       context
