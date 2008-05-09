7032e148a04f38fe0f50cc38f994ca1ffb790b65
Thomas Leitner <t_leitner@gmx.at>
2008-05-09 18:24:02 +0200

    Fixed SourceHandler::Base#output_path
    The method didn't return the full path including parent paths. This
    also lead to necessary changes in Node and Tree.

--- lib/webgen/default_config.rb
+++ lib/webgen/default_config.rb
@@ -24,7 +24,7 @@ config.sourcehandler.casefold(true, :doc => 'Specifies whether path are consider
 config.sourcehandler.use_hidden_files(false, :doc => 'Specifies whether hidden files (those starting with a dot) are used')
 config.sourcehandler.ignore(['**/*~', '**/.svn/**'], :doc => 'Path patterns that should be ignored')
 config.sourcehandler.default_lang_in_output_path(false, :doc => 'Specifies whether output paths in the default language should have the language in the name')
-config.sourcehandler.output_path_style([:cnbase, ['.', :lang], :ext], :doc => 'Defines how output path names are constructed from source path names')
+config.sourcehandler.output_path_style([:parent, :cnbase, ['.', :lang], :ext], :doc => 'Defines how output path names are constructed from source path names')
 =begin
 TODO:put this info into the user docs
       desc: Defines how the output name should be built. The correct name will be used for the
--- test/test_sourcehandler_base.rb
+++ test/test_sourcehandler_base.rb
@@ -18,7 +18,7 @@ class TestSourceHandlerBase < Test::Unit::TestCase
   def test_node_exists
     @tree = Webgen::Tree.new
     node = Webgen::Node.new(@tree.dummy_root, 'test/', 'test', {'lang' => 'de', :test => :value})
-    child_de = Webgen::Node.new(node, 'somename.html', 'somename.page', {'lang' => 'de'})
+    child_de = Webgen::Node.new(node, 'test/somename.html', 'somename.page', {'lang' => 'de'})
     frag_de = Webgen::Node.new(child_de, '#data1', '#othertest')
 
     assert_equal(child_de, @temp.node_exists?(node, Webgen::Path.new('somename.de.page')))
@@ -32,17 +32,22 @@ class TestSourceHandlerBase < Test::Unit::TestCase
     node = Webgen::Node.new(@tree.dummy_root, 'test/', 'test', {'lang' => 'de', :test => :value})
 
     path = Webgen::Path.new('path.html')
-    assert_equal('path.html', @temp.output_path(node, path, [:cnbase, ['.', :lang], :ext]))
+    assert_equal('test/path.html', @temp.output_path(node, path, [:parent, :cnbase, ['.', :lang], :ext]))
     path = Webgen::Path.new('path.en.html')
-    assert_equal('path.html', @temp.output_path(node, path, [:cnbase, ['.', :lang], :ext]))
+    assert_equal('test/path.html', @temp.output_path(node, path, [:parent, :cnbase, ['.', :lang], :ext]))
     path = Webgen::Path.new('path.eo.html')
-    assert_equal('path.eo.html', @temp.output_path(node, path, [:cnbase, ['.', :lang], :ext]))
+    assert_equal('test/path.eo.html', @temp.output_path(node, path, [:parent, :cnbase, ['.', :lang], :ext]))
+    path = Webgen::Path.new('dir/')
+    assert_equal('test/dir/', @temp.output_path(node, path, [:parent, :cnbase, ['.', :lang], :ext]))
 
-    other = Webgen::Node.new(node, 'path.html')
+    other = Webgen::Node.new(node, 'test/path.html', 'other.page')
     path = Webgen::Path.new('path.html')
-    assert_equal('path.html', @temp.output_path(node, path, [:cnbase, ['.', :lang], :ext]))
+    assert_equal('test/path.html', @temp.output_path(node, path, [:parent, :cnbase, ['.', :lang], :ext]))
     path = Webgen::Path.new('path.en.html')
-    assert_equal('path.en.html', @temp.output_path(node, path, [:cnbase, ['.', :lang], :ext]))
+    assert_equal('test/path.en.html', @temp.output_path(node, path, [:parent, :cnbase, ['.', :lang], :ext]))
+
+    path = Webgen::Path.new('/')
+    assert_equal('/', @temp.output_path(@tree.dummy_root, path, [:parent, :cnbase, ['.', :lang], :ext]))
   end
 
 end
