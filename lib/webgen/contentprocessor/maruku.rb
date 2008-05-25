f1e30e1888a5e4fd21b38c706781fbfaf135b032
Thomas Leitner <t_leitner@gmx.at>
2008-05-25 09:10:25 +0200

    Added some new tests
    The following classes have test cases now:
    * SourceHandler::Copy
    * SourceHandler::Directory
    * ContentProcessor::Maruku

--- lib/webgen/contentprocessor/maruku.rb
+++ lib/webgen/contentprocessor/maruku.rb
@@ -1,12 +1,11 @@
+require 'maruku'
+
 module Webgen::ContentProcessor
 
   class Maruku
 
-    include Webgen::WebsiteAccess
-
     def call(context)
-      require 'maruku'
-      context.content = ::Maruku.new(context.content).to_html
+      context.content = ::Maruku.new(context.content, :on_error => :error).to_html
       context
     rescue Exception => e
       raise "Maruku to HTML conversion failed: #{e.message}"
---
+++ test/test_sourcehandler_copy.rb
@@ -0,0 +1,45 @@
+require 'test/unit'
+require 'helper'
+require 'webgen/sourcehandler/copy'
+require 'stringio'
+
+class TestSourceHandlerCopy < Test::Unit::TestCase
+
+  include Test::WebsiteHelper
+
+  class TestCP
+    def call(context); context.content = context.content.reverse; end
+  end
+
+  def setup
+    super
+    @website.config['contentprocessor.map']['test'] = 'TestSourceHandlerCopy::TestCP'
+    @obj = Webgen::SourceHandler::Copy.new
+    @root = Webgen::Node.new(Webgen::Tree.new.dummy_root, 'test/', 'test')
+    @without = @obj.create_node(@root, path_with_meta_info('/default.css') {StringIO.new('# header')})
+    @with = @obj.create_node(@root, path_with_meta_info('/other.test.css') {StringIO.new('# header')})
+  end
+
+  def test_create_node
+    assert_not_nil(@without)
+    assert_equal(nil, @without.node_info[:preprocessor])
+    assert_equal('test/default.css', @without.path)
+
+    assert_not_nil(@with)
+    assert_equal('test', @with.node_info[:preprocessor])
+    assert_equal('test/other.css', @with.path)
+
+    node = @obj.create_node(@root, path_with_meta_info('/other.unknown.css') {StringIO.new('# header')})
+    assert_equal(nil, node.node_info[:preprocessor])
+  end
+
+  def test_content
+    @website.blackboard.add_service(:source_paths) do
+      {'/default.css' => path_with_meta_info('/default.css') {StringIO.new('# header')}}
+    end
+    @without.node_info[:src] = @with.node_info[:src] = '/default.css'
+    assert_kind_of(StringIO, @without.content)
+    assert_equal('# header'.reverse, @with.content)
+  end
+
+end
