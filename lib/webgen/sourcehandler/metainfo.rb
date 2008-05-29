09787926b3fc93e5065d1514b6e10402c6073359
Thomas Leitner <t_leitner@gmx.at>
2008-05-29 09:37:39 +0200

    Updates SourceHandler::Metainfo and added tests
    SH::Metainfo now uses files in Webgen Page Format and two different
    blocks: one for setting meta info on paths before a node is created
    and one for setting meta info on nodes via their alcn after the nodes
    are created.

--- lib/webgen/sourcehandler/metainfo.rb
+++ lib/webgen/sourcehandler/metainfo.rb
@@ -15,15 +15,19 @@ module Webgen::SourceHandler
       website.blackboard.add_listener(:node_changed?, method(:node_changed?))
       website.blackboard.add_listener(:before_node_created, method(:before_node_created))
       website.blackboard.add_listener(:before_node_deleted, method(:before_node_deleted))
+      website.blackboard.add_listener(:after_node_created, method(:after_node_created))
       self.nodes ||= Set.new
     end
 
     def create_node(parent, path)
+      page = page_from_path(path)
       super(parent, path) do |node|
-        node.node_info[:data] = {}
-        YAML::load(path.io.read).each do |key, value|
-          key = File.expand_path(key =~ /^\// ? key : parent.absolute_lcn + key)
-          node.node_info[:data][key] = value
+        [[:mi_paths, 'paths'], [:mi_alcn, 'alcn']].each do |mi_key, block_name|
+          node.node_info[mi_key] = {}
+          YAML::load(page.blocks[block_name].content).each do |key, value|
+            key = File.expand_path(key =~ /^\// ? key : parent.absolute_lcn + key)
+            node.node_info[mi_key][key.chomp('/')] = value
+          end if page.blocks.has_key?(block_name)
         end
 
         mark_all_matched_dirty(node)
@@ -43,22 +47,34 @@ module Webgen::SourceHandler
     def mark_all_matched_dirty(node)
       source_paths = website.blackboard.invoke(:source_paths)
       node.tree.node_access[:alcn].each do |path, n|
-        n.dirty = true if node.node_info[:data].any? {|pattern, mi| source_paths[n.node_info[:src]] =~ pattern }
+        n.dirty = true if node.node_info[:mi_paths].any? {|pattern, mi| source_paths[n.node_info[:src]] =~ pattern }
+        n.dirty = true if node.node_info[:mi_alcn].any? {|pattern, mi| n =~ pattern }
       end
     end
 
     def before_node_created(parent, path)
       self.nodes.each do |node|
-        node.node_info[:data].each do |pattern, mi|
+        node.node_info[:mi_paths].each do |pattern, mi|
           path.meta_info.update(mi) if path =~ pattern
         end
       end
     end
 
+    def after_node_created(node)
+      self.nodes.each do |n|
+        n.node_info[:mi_alcn].each do |pattern, mi|
+          node.meta_info.update(mi) if node =~ pattern
+        end
+      end
+    end
+
     def node_changed?(node)
+      return if self.nodes.include?(node)
       path = website.blackboard.invoke(:source_paths)[node.node_info[:src]]
       self.nodes.each do |n|
-        if n.node_info[:data].any? {|pattern,mi| path =~ pattern} && n.changed?
+        if n.changed? &&
+            (n.node_info[:mi_paths].any? {|pattern, mi| path =~ pattern} ||
+             n.node_info[:mi_alcn].any? {|pattern, mi| node =~ pattern})
           node.dirty = true
           return
         end
