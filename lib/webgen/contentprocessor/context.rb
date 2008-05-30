473b6e3acd138bc75c856ad40fa8b7728632a4c2
Thomas Leitner <t_leitner@gmx.at>
2008-05-30 17:04:54 +0200

    Added ContentProcessor::Context#dest_node and API docs

--- lib/webgen/contentprocessor/context.rb
+++ lib/webgen/contentprocessor/context.rb
@@ -35,10 +35,28 @@ module Webgen::ContentProcessor
       @options[:content] = value
     end
 
+    # Returns the node which represents the file into which everything gets rendered. This is
+    # normally the same node as <tt>#content_node</tt> but can differ in special cases. For example,
+    # when rendering the content of node called +my.page+ into the output of the node +this.page+,
+    # +this.page+ would be the +dest_node+ and +my.page+ would be the +content_node+.
+    #
+    # The +dest_node+ is not included in the chain!
+    #
+    # This node should be used as source node for calculating relative paths to other nodes.
+    def dest_node
+      @options[:dest_node] || self.content_node
+    end
+
+    # Returns the reference node, ie. the node which provided the original content for this context
+    # object.
+    #
+    # This node should be used, for example, for resolving relative paths.
     def ref_node
       @options[:chain] && @options[:chain].first
     end
 
+    # Returns the node that is ultimately rendered. This node should be used, for example, for
+    # retrieving meta information.
     def content_node
       @options[:chain] && @options[:chain].last
     end
