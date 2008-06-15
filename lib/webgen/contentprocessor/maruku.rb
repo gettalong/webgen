c2e8536edb924c5f9e9dffd5019c2fe758b205d2
Thomas Leitner <t_leitner@gmx.at>
2008-06-15 09:07:36 +0200

    ContentProcessor::Maruku now raises exceptions on parse errors

--- lib/webgen/contentprocessor/maruku.rb
+++ lib/webgen/contentprocessor/maruku.rb
@@ -4,7 +4,7 @@ module Webgen::ContentProcessor
 
     def call(context)
       require 'maruku'
-      context.content = ::Maruku.new(context.content, :on_error => :error).to_html
+      context.content = ::Maruku.new(context.content, :on_error => :raise).to_html
       context
     rescue Exception => e
       raise "Maruku to HTML conversion failed: #{e.message}"
