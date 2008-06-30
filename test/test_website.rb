e8ba8c08a07bc5a25dcfec28953d6ab9cfa5db14
Thomas Leitner <t_leitner@gmx.at>
2008-06-30 10:20:37 +0200

    Small update to Website
    Website#new now takes the website directory as mandatory argument
    and all website specific extensions named *_init.rb are loaded
    on Website#init.

--- test/test_website.rb
+++ test/test_website.rb
@@ -5,19 +5,21 @@ require 'webgen/website'
 class TestWebsite < Test::Unit::TestCase
 
   def test_initialize
-    ws = Webgen::Website.new do
+    ws = Webgen::Website.new('dir') do
       assert_equal(ws, Webgen::WebsiteAccess.website)
       throw :called
     end
-    assert_not_nil(ws.blackboard)
+    assert_nil(ws.blackboard)
     assert_nil(ws.cache)
     assert_nil(ws.config)
+    assert_equal('dir', ws.directory)
     assert_throws(:called) { ws.init }
     assert_nil(Thread.current[:webgen_website])
   end
 
   def test_autoload_service
-    ws = Webgen::Website.new
+    ws = Webgen::Website.new(nil)
+    ws.init
     ws.instance_eval { @cache = Webgen::Cache.new }
     ws.autoload_service('[]', 'Hash')
     assert_equal([], ws.cache.permanent[:classes])
@@ -26,15 +28,14 @@ class TestWebsite < Test::Unit::TestCase
   end
 
   def test_init
-    ws = Webgen::Website.new {|config| config['website.dir'] = 'hallo' }
+    ws = Webgen::Website.new('hallo')
     ws.init
     assert_not_nil(ws.config)
     assert_equal('hallo', ws.config['website.dir'])
   end
 
   def test_render
-    ws = Webgen::Website.new do |config|
-      config['website.dir'] = File.dirname(__FILE__)
+    ws = Webgen::Website.new(File.dirname(__FILE__)) do |config|
       config['website.cache'] = [:string, '']
     end
     ws.logger = nil
