7343822a18d23805765ede3ba64eaec862f4fdb2
Thomas Leitner <t_leitner@gmx.at>
2008-07-10 07:16:39 +0200

    Added basic test for WebgenTask

---
+++ test/test_webgentask.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require 'webgen/webgentask'
+
+class TestWebgenTask < Test::Unit::TestCase
+
+  include Rake
+
+  def test_create
+    site = Webgen::WebgenTask.new('doit') do |s|
+      s.clobber_outdir = true
+      s.config_block = lambda {|c| c}
+      s.directory = 'website'
+    end
+    assert_equal('website', site.directory)
+    assert_equal(true, site.clobber_outdir)
+    assert_equal(5, site.config_block[5])
+    assert(Task['doit'])
+    assert(Task['clobber_doit'])
+  end
+
+end
