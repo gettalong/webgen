e65bfda939ed931c89894726c8c29d4422b7c7df
Thomas Leitner <t_leitner@gmx.at>
2008-08-11 13:02:52 +0200

    Some adjustments to the layout

--- data/webgen/webgui/controller/main.rb
+++ data/webgen/webgui/controller/main.rb
@@ -5,17 +5,24 @@ class MainController < Ramaze::Controller
   layout '/page'
 
   def site_ops
-    if request['set_site']
-      session['website_dir'] = request['site_dir']
-    elsif request['render_site'] && session['website_dir']
+    session['website_dir'] = request['site_dir'] if request.post? && request['set_site']
+
+    if File.directory?(session['website_dir'])
+      redirect R(:manage_website)
+    else
+      redirect R(:create_website)
+    end
+  end
+
+  def manage_website
+    if request['render_site'] && session['website_dir']
       Webgen::Website.new(session['website_dir']).render
     elsif request['auto_render_site'] && session['website_dir']
       #TODO
     end
-    redirect R('/')
   end
 
-  def website(*args)
+  def preview_website(*args)
     ws = Webgen::Website.new(session['website_dir'])
     ws.init
     ws.execute_in_env do
