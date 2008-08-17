89a4c9f9f71a603666584dd639fe51d6cb0be09d
Thomas Leitner <t_leitner@gmx.at>
2008-08-17 08:49:19 +0200

    Updated test cases for virtual source handler

--- test/test_sourcehandler_virtual.rb
+++ test/test_sourcehandler_virtual.rb
@@ -25,7 +25,8 @@ EOF
     obj = Webgen::SourceHandler::Virtual.new
     root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/')
     shm = Webgen::SourceHandler::Main.new # for using service :create_nodes
-    nodes = obj.create_node(root, path_with_meta_info('/virtuals', {}, obj.class.name) {StringIO.new(CONTENT)})
+    time = Time.now
+    nodes = obj.create_node(root, path_with_meta_info('/virtuals', {'modified_at' => time}, obj.class.name) {StringIO.new(CONTENT)})
 
     nodes.each {|n| assert_equal('/virtuals', n.node_info[:src])}
 
@@ -37,6 +38,8 @@ EOF
     assert_not_nil(path_en)
 
     assert_equal('new title', path_en['title'])
+    assert_equal(time, path_en['modified_at'])
+    assert(path_en['no_output'])
     assert_equal('My Dir', dir['title'])
     assert_equal('directory/other.html', path_de.route_to(path_en))
     assert_equal('../path.de.html', dir.route_to(path_de))
