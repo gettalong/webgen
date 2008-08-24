4ba332937fc7350ba61a618f2d91495679922449
Thomas Leitner <t_leitner@gmx.at>
2008-08-24 16:42:29 +0200

    Small gem spec fixes

---
+++ data/webgen/website_skeleton/ext/init.rb
@@ -0,0 +1,6 @@
+# = webgen extensions directory
+#
+# All init.rb files anywhere under this directory get automatically loaded on a webgen run. This
+# allows you to add your own extensions to webgen or to modify webgen's core!
+#
+# If you don't need this feature you can savely delete this file and the directory in which it is!
