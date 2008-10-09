4e08a18aa6f852d02d890efaade15df169cb5e48
Thomas Leitner <t_leitner@gmx.at>
2008-10-09 08:04:30 +0200

    Added sourcehandler Memory for nodes created during write phase
    This source handler should be used for nodes that are generated
    during the write phase and which can only be reinitialized/recreated
    when their source node gets written again.

---
+++ test/test_sourcehandler_memory.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require 'helper'
+require 'webgen/sourcehandler'
+require 'stringio'
+
+class TestSourceHandlerMemory < Test::Unit::TestCase
+
+  include Test::WebsiteHelper
+
+  def test_all
+    obj = Webgen::SourceHandler::Memory.new
+    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/')
+    shm = Webgen::SourceHandler::Main.new # for using service :create_nodes
+    root.unflag(:dirty)
+    root.unflag(:created)
+
+    node = obj.create_node(root, path_with_meta_info('/test.png'), '/', 'data')
+    assert_equal('/', node.node_info[:memory_source_alcn])
+    assert_equal('data', obj.content(node))
+    assert(!node.flagged(:reinit))
+    root.tree.delete_node(node)
+
+    node = obj.create_node(root, path_with_meta_info('/test.png'), '/') {|n| assert_equal(node, n); 'data'}
+    assert_equal('/', node.node_info[:memory_source_alcn])
+    assert_equal('data', obj.content(node))
+    assert(!node.flagged(:reinit))
+
+    assert(!root.flagged(:dirty))
+    node.flag(:reinit)
+    assert(root.flagged(:dirty))
+    root.unflag(:dirty)
+    root.tree.delete_node(node)
+
+    node = obj.create_node(root, path_with_meta_info('/test.png'), '/', 'data')
+    assert_equal('/', node.node_info[:memory_source_alcn])
+    obj.instance_eval { @data = nil }
+    assert_nil(obj.content(node))
+    assert(node.flagged(:reinit))
+    assert(root.flagged(:dirty))
+  end
+
+end
