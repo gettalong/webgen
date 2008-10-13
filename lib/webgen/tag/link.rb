c770c8d5967bb0d0c9ec95767baac6804b2e5554
Thomas Leitner <t_leitner@gmx.at>
2008-10-13 07:58:22 +0200

    Added new link tag for generating links

---
+++ lib/webgen/tag/link.rb
@@ -0,0 +1,23 @@
+module Webgen::Tag
+
+  # Create a link to a given (A)LCN.
+  class Link
+
+    include Webgen::Tag::Base
+
+    # Return a HTML link to the given (A)LCN.
+    def call(tag, body, context)
+      if (dest_node = context.ref_node.resolve(param('tag.link.path'), context.dest_node.lang))
+        context.dest_node.link_to(dest_node, param('tag.link.attr').merge(:lang => context.content_node.lang))
+      else
+        raise ArgumentError, 'Resolving of path failed'
+      end
+    rescue ArgumentError, URI::InvalidURIError => e
+      log(:error) { "Could not link to path '#{param('tag.link.path')}' in <#{context.ref_node.absolute_lcn}>: #{e.message}" }
+      context.dest_node.flag(:dirty)
+      ''
+    end
+
+  end
+
+end
---
+++ test/test_tag_link.rb
@@ -0,0 +1,61 @@
+require 'test/unit'
+require 'helper'
+require 'webgen/tree'
+require 'webgen/contentprocessor'
+require 'webgen/tag'
+
+class TestTagLink < Test::Unit::TestCase
+
+  include Test::WebsiteHelper
+
+  def setup
+    super
+    @obj = Webgen::Tag::Link.new
+  end
+
+  def call(context)
+    @obj.call('link', '', context)
+  end
+
+  def test_call
+    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/')
+    node = Webgen::Node.new(root, '/file.html', 'file.html', {'lang' => 'en'})
+    dir = Webgen::Node.new(root, '/dir/', 'dir/', 'index_path' => "index.html")
+    file = Webgen::Node.new(dir, '/dir/file.html', 'file.html', {'lang' => 'en', 'title' => 'Dir/File'})
+    Webgen::Node.new(dir, '/dir/other.de.html', 'other.html', {'lang' => 'de'})
+    Webgen::Node.new(file, '/dir/file.html#fragment', '#fragment')
+    dir2 = Webgen::Node.new(root, '/dir2/', 'dir2/', 'index_path' => "index.html")
+    Webgen::Node.new(dir2, '/dir2/index.html', 'index.html')
+
+    context = Webgen::ContentProcessor::Context.new(:chain => [node])
+
+    # no path set
+    node.unflag(:dirty)
+    @obj.set_params('tag.link.path' => nil)
+    assert_equal('', call(context))
+    assert(node.flagged(:dirty))
+
+    # invalid paths
+    @obj.set_params('tag.link.path' => ':/asdf=-)')
+    assert_equal('', call(context))
+
+    # basic node resolving
+    @obj.set_params('tag.link.path' => 'dir/file.html')
+    assert_equal('<a href="dir/file.html">Dir/File</a>', call(context))
+    @obj.set_params('tag.link.path' => 'dir/file.html', 'tag.link.attr' => {'title' => 'other'})
+    assert_equal('<a href="dir/file.html" title="other">Dir/File</a>', call(context))
+    @obj.set_params('tag.link.path' => 'dir/other.html')
+    assert_equal('', call(context))
+
+    # non-existing fragments
+    @obj.set_params('tag.link.path' => 'file.html#hallo')
+    assert_equal('', call(context))
+
+    # directory paths
+    @obj.set_params('tag.link.path' => 'dir')
+    assert_equal('<a href="dir/"></a>', call(context))
+    @obj.set_params('tag.link.path' => 'dir2')
+    assert_equal('<a href="dir2/index.html"></a>', call(context))
+  end
+
+end
