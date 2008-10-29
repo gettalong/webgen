bca1718cc7a24f011cf9c051010e3a0836dfe646
Thomas Leitner <t_leitner@gmx.at>
2008-10-29 07:38:31 +0100

    Factored out method for creating an absolute path

--- lib/webgen/common.rb
+++ lib/webgen/common.rb
@@ -1,9 +1,18 @@
+require 'pathname'
+
 module Webgen
 
-  # Namespace for classes that provide common functionality.
+  # Namespace for classes and methods that provide common functionality.
   module Common
 
     autoload :Sitemap, 'webgen/common/sitemap'
+
+    # Make the given +path+ absolute by prepending the absolute path +base+ if necessary. Also
+    # resolves all '..'  and '.' references in +path+.
+    def self.absolute_path(path, base)
+      raise(ArgumentError, 'base has to be an absolute path') unless base =~ /\//
+      Pathname.new(path =~ /^\// ? path : File.join(base, path)).cleanpath.to_s
+    end
 
   end
 
---
+++ test/test_common.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require 'webgen/common'
+
+class TestCommon < Test::Unit::TestCase
+
+  def test_absolute_path
+    assert_raise(ArgumentError) { Webgen::Common.absolute_path('test', 'test') }
+    assert_equal('/', Webgen::Common.absolute_path('/', '/'))
+    assert_equal('/dir', Webgen::Common.absolute_path('/dir', '/other'))
+    assert_equal('/other/dir', Webgen::Common.absolute_path('dir', '/other'))
+    assert_equal('/test/dir', Webgen::Common.absolute_path('../test/dir', '/other'))
+    assert_equal('/', Webgen::Common.absolute_path('/..', '/'))
+    assert_equal('/dir', Webgen::Common.absolute_path('/../dir/.', '/'))
+  end
+
+end
