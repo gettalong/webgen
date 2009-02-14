fb5f3a3b3046b19759f55a13ec10e3d2828f6244
Thomas Leitner <t_leitner@gmx.at>
2009-02-14 16:12:03 +0100

    Implemented two new features for ContentProcessor::Blocks
    * Added support for optional attribute node for specifying which node in
      the chain should be used (next node or first node with such a block).
    * Added support for ignoring errors and failing silently

--- lib/webgen/contentprocessor/blocks.rb
+++ lib/webgen/contentprocessor/blocks.rb
@@ -5,7 +5,8 @@ module Webgen::ContentProcessor
 
     include Webgen::Loggable
 
-    BLOCK_RE = /<webgen:block\s+name=('|")(\w+)\1\s*(?:\schain=('|")([^'"]+)\3\s*)?\s*\/>/
+    BLOCK_RE = /<webgen:block\s*?((?:\s\w+=('|")[^'"]+\2)+)\s*\/>/
+    BLOCK_ATTR_RE = /(\w+)=('|")([^'"]+)\2/
 
     # Replace that webgen:block xml tags with the rendered content of a node.
     def call(context)
@@ -13,11 +14,12 @@ module Webgen::ContentProcessor
       new_chain = (chain.length > 1 ? chain[1..-1] : chain)
 
       context.content.gsub!(BLOCK_RE) do |match|
-        block_name = $2
-        if $4.nil?
-          used_chain = new_chain
+        attr = {}
+        match.scan(BLOCK_ATTR_RE) {|name, sep, content| attr[name] = content}
+        if attr['chain'].nil?
+          used_chain = new_chain.dup
         else
-          paths = $4.split(';')
+          paths = attr['chain'].split(';')
           used_chain = paths.collect do |path|
             temp_node = context.ref_node.resolve(path.strip, context.dest_node.lang)
             log(:error) { "Could not resolve <#{path.strip}> in <#{context.ref_node.absolute_lcn}> in '#{match.to_s}'" } if temp_node.nil?
@@ -26,14 +28,24 @@ module Webgen::ContentProcessor
           next match if used_chain.length != paths.length
           dest_node = context.content_node
         end
+
+        if attr['node'] == 'first'
+          used_chain.shift while used_chain.length > 0 && !used_chain.first.node_info[:page].blocks.has_key?(attr['name'])
+        end
         block_node = used_chain.first
 
-        if !block_node.node_info[:page].blocks.has_key?(block_name)
-          raise "Node <#{block_node.absolute_lcn}> has no block named '#{block_name}'"
+        if !block_node || !block_node.node_info[:page].blocks.has_key?(attr['name'])
+          if attr['notfound'] == 'ignore'
+            next ''
+          elsif block_node
+            raise "Node <#{block_node.absolute_lcn}> has no block named '#{attr['name']}'"
+          else
+            raise "No node in the chain has a block named '#{attr['name']}'"
+          end
         end
 
         context.dest_node.node_info[:used_nodes] << block_node.absolute_lcn
-        tmp_context = block_node.node_info[:page].blocks[block_name].render(context.clone(:chain => used_chain, :dest_node => dest_node))
+        tmp_context = block_node.node_info[:page].blocks[attr['name']].render(context.clone(:chain => used_chain, :dest_node => dest_node))
         tmp_context.content
       end
       context
