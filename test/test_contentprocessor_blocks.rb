3a4ec10a91dc52b5f49d8f62006f230bfb2aaab0
Thomas Leitner <t_leitner@gmx.at>
2009-02-14 16:12:03 +0100

    Implemented two new features for ContentProcessor::Blocks
    * Added support for optional attribute node for specifying which node in
      the chain should be used (next node or first node with such a block).
    * Added support for ignoring errors and failing silently

--- test/test_contentprocessor_blocks.rb
+++ test/test_contentprocessor_blocks.rb
@@ -9,7 +9,7 @@ class TestContentProcessorBlocks < Test::Unit::TestCase
     obj = Webgen::ContentProcessor::Blocks.new
     root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/')
     node = Webgen::Node.new(root, 'test', 'test')
-    node.node_info[:page] = Webgen::Page.from_data("--- name:content\ndata")
+    node.node_info[:page] = Webgen::Page.from_data("--- name:content\ndata\n--- name:other\nother")
     template = Webgen::Node.new(root, 'template', 'template')
     template.node_info[:page] = Webgen::Page.from_data("--- name:content pipeline:blocks\nbefore<webgen:block name='content' />after")
     processors = { 'blocks' => obj }
@@ -29,6 +29,25 @@ class TestContentProcessorBlocks < Test::Unit::TestCase
     obj.call(context)
     assert_equal('<webgen:block name="content" chain="invalid" />beforedataafter', context.content)
     assert_equal(Set.new([template.absolute_lcn, node.absolute_lcn]), node.node_info[:used_nodes])
+
+    context.content = 'bef<webgen:block name="other" chain="template;test" notfound="ignore" />aft'
+    obj.call(context)
+    assert_equal('befaft', context.content)
+
+    context.content = '<webgen:block name="other" chain="template" node="first" />'
+    assert_raise(RuntimeError) { obj.call(context) }
+
+    context.content = '<webgen:block name="other" chain="template;test" node="first" />'
+    obj.call(context)
+    assert_equal('other', context.content)
+
+    context.content = '<webgen:block name="other" chain="test" node="first" />'
+    obj.call(context)
+    assert_equal('other', context.content)
+
+    context.content = '<webgen:block name="invalid" node="first" notfound="ignore" /><webgen:block name="content" />'
+    obj.call(context)
+    assert_equal('beforedataafter', context.content)
   end
 
 end
