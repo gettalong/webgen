833924035cf974aa79277e81c5bfd5368be3be00
Thomas Leitner <t_leitner@gmx.at>
2009-02-16 16:12:01 +0100

    Small update to website skeleton

--- data/webgen/website_skeleton/ext/init.rb
+++ data/webgen/website_skeleton/ext/init.rb
@@ -4,3 +4,7 @@
 # allows you to add your own extensions to webgen or to modify webgen's core!
 #
 # If you don't need this feature you can savely delete this file and the directory in which it is!
+#
+# The +config+ variable below can be used to access the Webgen::Configuration object for the current
+# website.
+config = Webgen::WebsiteAccess.website.config
