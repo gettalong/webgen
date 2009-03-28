c2b5435d8ae3026d3553866a103a4fec9f4fb7f7
Thomas Leitner <t_leitner@gmx.at>
2009-03-28 19:34:48 +0100

    Updated needed ramaze version to 2009.02 and made ramaze and launchy development deps

--- lib/webgen/cli/webgui_command.rb
+++ lib/webgen/cli/webgui_command.rb
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 require 'webgen/cli'
+require 'facets/kernel/silence'
 
 module Webgen::CLI
 
@@ -14,32 +15,48 @@ module Webgen::CLI
 
     # Render the website.
     def execute(args)
-      # some fixes for ramaze-2008.06
-      # - fix for Windows bug when win32console is not installed
+      # some fixes for ramaze-2009.02
+      # - fix for Windows when win32console is not installed
       # - fix for message displayed on shutdown
+      # - fix for warning message
       $:.unshift File.join(Webgen.data_dir, 'webgui', 'overrides')
       require 'win32console'
       $:.shift
-      require 'ramaze'
-      Ramaze::Log.loggers = []
+      silence_warnings do
+        begin
+          require 'ramaze'
+        rescue LoadError
+          puts "The Ramaze web framework which is needed for the webgui was not found."
+          puts "You can install it via 'gem install ramaze --version 2009.02'"
+          return
+        end
+      end
       def Ramaze.shutdown; # :nodoc:
       end
 
-      acquire Webgen.data_dir/:webgui/:controller/'*'
+      Ramaze::acquire(File.join(Webgen.data_dir, 'webgui', 'controller', '*'))
+      Ramaze::Log.loggers = []
       Ramaze::Global.setup do |g|
-        g.root = Webgen.data_dir/:webgui
-        g.public_root = Webgen.data_dir/:webgui/:public
-        g.view_root = Webgen.data_dir/:webgui/:view
+        g.root = File.join(Webgen.data_dir, 'webgui')
+        g.public_root = File.join(Webgen.data_dir, 'webgui', 'public')
+        g.view_root = File.join(Webgen.data_dir, 'webgui', 'view')
         g.adapter = :webrick
         g.port = 7000
       end
 
       puts 'Starting webgui on http://localhost:7000, press Control-C to stop'
+
       Thread.new do
-        sleep 2
-        puts 'Launching web browser'
-        require 'launchy'
-        Launchy.open('http://localhost:7000')
+        begin
+          require 'launchy'
+          sleep 2
+          puts 'Launching web browser'
+          Launchy.open('http://localhost:7000')
+        rescue LoadError
+          puts "Can't open browser because the launchy library was not found."
+          puts "You can install it via 'gem install launchy'"
+          puts "Please open a browser window and enter 'http://localhost:7000' into the address bar!"
+        end
       end
 
       Ramaze.start
