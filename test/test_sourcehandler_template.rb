aa7a76159069e7aac48e91a8abcb503dba619394
Thomas Leitner <t_leitner@gmx.at>
2009-06-03 18:13:38 +0200

    Updates to tests to work again after changes to Node/Tree/Path

--- test/test_sourcehandler_template.rb
+++ test/test_sourcehandler_template.rb
@@ -27,14 +27,14 @@ class TestSourceHandlerTemplate < Test::Unit::TestCase
   end
 
   def test_templates_for_node
-    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, 'test/', 'test', {'lang' => 'de', :test => :value})
-    default_template = Webgen::Node.new(root, 'default.template', 'default.template')
-    default_de_template = Webgen::Node.new(root, 'default.de.template', 'default.template', {'lang' => 'de'})
-    other_template = Webgen::Node.new(root, 'other.template', 'other.template')
-    stopped_template = Webgen::Node.new(root, 'stopped.html', 'stopped.page', { 'template' => nil})
-    invalid_template = Webgen::Node.new(root, 'invalid.template', 'invalid.template', {'template' => 'invalidity'})
-    chained_template = Webgen::Node.new(root, 'chained.template', 'chained.template', {'template' => 'other.template'})
-    german_file = Webgen::Node.new(root, 'german.html', 'german.page', {'lang' => 'de', 'template' => 'other.template'})
+    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/', {:test => :value})
+    default_template = Webgen::Node.new(root, '/default.template', 'default.template')
+    default_de_template = Webgen::Node.new(root, '/default.de.template', 'default.template', {'lang' => 'de'})
+    other_template = Webgen::Node.new(root, '/other.template', 'other.template')
+    stopped_template = Webgen::Node.new(root, '/stopped.html', 'stopped.page', { 'template' => nil})
+    invalid_template = Webgen::Node.new(root, '/invalid.template', 'invalid.template', {'template' => 'invalidity'})
+    chained_template = Webgen::Node.new(root, '/chained.template', 'chained.template', {'template' => 'other.template'})
+    german_file = Webgen::Node.new(root, '/german.html', 'german.page', {'lang' => 'de', 'template' => 'other.template'})
     dir = Webgen::Node.new(root, '/dir/', 'dir')
     dir_default_template = Webgen::Node.new(dir, '/dir/default.template', 'default.template')
 
@@ -52,9 +52,9 @@ class TestSourceHandlerTemplate < Test::Unit::TestCase
   end
 
   def test_default_template
-    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, 'test/', 'test', {'lang' => 'de', :test => :value})
-    dir = Webgen::Node.new(root, 'dir/', 'dir')
-    template = Webgen::Node.new(root, 'default.template', 'default.template')
+    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/', {:test => :value})
+    dir = Webgen::Node.new(root, '/dir/', 'dir/')
+    template = Webgen::Node.new(root, '/default.template', 'default.template')
 
     assert_equal(template, @obj.default_template(root, nil))
     assert_equal(template, @obj.default_template(dir, nil))
