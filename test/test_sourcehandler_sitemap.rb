b9e0b5966a0dccd3c7a0b631b3cbc8714fd4a037
Thomas Leitner <t_leitner@gmx.at>
2009-06-25 21:57:44 +0200

    Updated SH::Sitemap to also use passive sources
    This also means that the sitemap source handler can now be used without
    the builder library. Another thing is that a sitemap file is now similar
    to a feed file because the template for the generation of a sitemap can
    now be specified directly in the sitemap file.

--- test/test_sourcehandler_sitemap.rb
+++ test/test_sourcehandler_sitemap.rb
@@ -15,8 +15,19 @@ site_url: http://example.com
 default_change_freq: daily
 EOF
 
+  SITEMAP_CONTENT_TEMPLATE = <<EOF
+---
+site_url: http://example.com
+default_change_freq: daily
+--- name:template
+Yeah <%= context.node['title'] %>
+EOF
+
   def setup
     super
+    shm = Webgen::SourceHandler::Main.new
+    @website.blackboard.del_listener(:node_meta_info_changed?, shm.method(:meta_info_changed?))
+    @website.config['passive_sources'] << ['/', "Webgen::Source::Resource", "webgen-passive-sources"]
     @nodes = create_sitemap_nodes
     @nodes.each {|k,v| v['modified_at'] = Time.now}
     @nodes[:file11_en]['priority'] = 0.9
@@ -35,6 +46,14 @@ EOF
     assert_raise(RuntimeError) do
       @obj.create_node(path_with_meta_info('/test.sitemap', {}, @obj.class.name) {StringIO.new('')})
     end
+  end
+
+  def test_create_node_with_own_template
+    @path = path_with_meta_info('/test.sitemap', {}, @obj.class.name) {StringIO.new(SITEMAP_CONTENT_TEMPLATE)}
+    sitemap = @obj.create_node(@path)
+    sitemap['title'] = 'test'
+    assert_not_nil(sitemap)
+    assert_equal('Yeah test', sitemap.content)
   end
 
   def test_content
