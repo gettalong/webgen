4cc42a5419dd009e8288af41a3c8927fb1360cae
Thomas Leitner <t_leitner@gmx.at>
2009-07-03 15:46:18 +0200

    Updated Tag::Coderay to allow the inclusion of external stylesheets
    The new options tag.coderay.css allows one to select from either using
    inline styles, the default external coderay stylesheet or to provide
    the styles in an other way (ie. custom stylesheet).

--- test/test_tag_coderay.rb
+++ test/test_tag_coderay.rb
@@ -13,20 +13,30 @@ class TestTagCoderay < Test::Unit::TestCase
     @obj = Webgen::Tag::Coderay.new
   end
 
-  def call(context, body, lang, process)
+  def call(context, body, lang, process, css = 'style')
     @obj.set_params({'tag.coderay.lang' => lang,
-                      'tag.coderay.process' => process})
+                      'tag.coderay.process' => process,
+                      'tag.coderay.css' => css})
     result = @obj.call('coderay', body, context)
     @obj.set_params({})
     result
   end
 
   def test_call
-    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/', {'title' => 'Hallo'})
+    root = Webgen::Node.new(@website.tree.dummy_root, '/', '/', {'title' => 'Hallo'})
     context = Webgen::Context.new(:chain => [root])
 
     assert(call(context, 'TestData', 'html', false).include?('TestData'))
     assert(call(context, '{title:}', :ruby, true).include?('Hallo'))
+
+    assert(call(context, 'TestData', 'ruby', false, 'other').include?('class="co"'))
+    assert(!@website.tree['/stylesheets/coderay-default.css'])
+
+    @website.blackboard.del_listener(:node_meta_info_changed?, Webgen::SourceHandler::Main.new.method(:meta_info_changed?))
+    @website.config['passive_sources'] << ['/', "Webgen::Source::Resource", "webgen-passive-sources"]
+    assert(call(context, 'TestData', 'ruby', false, 'class').include?('class="co"'))
+    assert(@website.tree['/stylesheets/coderay-default.css'])
+    assert_equal('stylesheets/coderay-default.css', context.persistent[:cp_head][:css_file].first)
   end
 
 end
