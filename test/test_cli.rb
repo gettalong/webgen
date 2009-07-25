d1187963388c74e9c9cf65dc8baf82026cccaefb
Thomas Leitner <t_leitner@gmx.at>
2009-07-25 07:45:26 +0200

    Small fix for CLI tests on Windows

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -4,6 +4,7 @@ require 'test/unit'
 require 'webgen/cli'
 require 'tmpdir'
 require 'fileutils'
+require 'rbconfig'
 
 class TestCLICommandParser < Test::Unit::TestCase
 
@@ -16,99 +17,103 @@ class TestCLICommandParser < Test::Unit::TestCase
 
 end
 
-class TestCLICommands < Test::Unit::TestCase
+if Config::CONFIG['host_os'] !~ /mswin|mingw/
 
-  def setup
-    @dir = File.join(Dir.tmpdir, "webgen-#{Process.pid}")
-    @cli = Webgen::CLI::CommandParser.new
-  end
+  class TestCLICommands < Test::Unit::TestCase
 
-  def reroute
-    sinread, sinwrite = IO.pipe
-    soutread, soutwrite = IO.pipe
-    aread, awrite = IO.pipe
-    awrite.reopen($stdin)
-    aread.reopen($stdout)
-    IO.new(0).reopen(sinread)
-    IO.new(1).reopen(soutwrite)
-    yield(sinwrite, soutread, sinread, soutwrite)
-    sinwrite.close_write
-    soutwrite.close_write
-    soutread
-  ensure
-    IO.new(0).reopen(awrite)
-    IO.new(1).reopen(aread)
-  end
-
-  def test_reroute
-    soutr = reroute do |sinw, soutr, sinr, soutw|
-      soutw.puts "test"
+    def setup
+      @dir = File.join(Dir.tmpdir, "webgen-#{Process.pid}")
+      @cli = Webgen::CLI::CommandParser.new
     end
-    assert_equal("test\n", soutr.read)
-  end
 
-  def teardown
-    FileUtils.rm_rf(@dir)
-  end
-
-  def test_create_simple
-    @cli.parse(['create', @dir])
-    assert(File.directory?(@dir))
-    assert(File.file?(File.join(@dir, 'config.yaml')))
-  end
-
-  def test_create_with_bundles
-    @cli.parse(['create', '-b', 'default', '-b', '07', @dir])
-    assert(File.directory?(@dir))
-    assert(File.file?(File.join(@dir, 'config.yaml')))
-    assert(File.file?(File.join(@dir, 'src', 'default.template')))
-  end
-
-  def test_create_with_no_bundle
-    @cli.parse(['create', '-b', 'none', @dir])
-    assert(File.directory?(@dir))
-    assert(File.file?(File.join(@dir, 'config.yaml')))
-    assert(!File.file?(File.join(@dir, 'src', 'default.template')))
-  end
-
-  def test_apply
-    assert_raise(RuntimeError) { @cli.parse(['-d', @dir, 'apply', '07']) }
-    @cli.parse(['create', '-b', 'none', @dir])
-    assert_raise(SystemExit) { reroute { @cli.parse(['-d', @dir, 'apply']) }}
-    reroute do |sinwrite, soutread|
-      sinwrite.puts "no"
-      @cli.parse(['-d', @dir, 'apply', '07'])
+    def reroute
+      sinread, sinwrite = IO.pipe
+      soutread, soutwrite = IO.pipe
+      aread, awrite = IO.pipe
+      awrite.reopen($stdin)
+      aread.reopen($stdout)
+      IO.new(0).reopen(sinread)
+      IO.new(1).reopen(soutwrite)
+      yield(sinwrite, soutread, sinread, soutwrite)
+      sinwrite.close_write
+      soutwrite.close_write
+      soutread
+    ensure
+      IO.new(0).reopen(awrite)
+      IO.new(1).reopen(aread)
     end
-    assert(!File.file?(File.join(@dir, 'src', 'default.template')))
-    reroute do |sinwrite, soutread|
-      sinwrite.puts "yes"
-      @cli.parse(['-d', @dir, 'apply', '07'])
-    end
-    assert(File.file?(File.join(@dir, 'src', 'default.template')))
-  end
 
-  def test_apply_forced
-    @cli.parse(['create', '-b', 'none', @dir])
-    reroute { @cli.parse(['-d', @dir, 'apply', '-f', '07']) }
-    assert(File.file?(File.join(@dir, 'src', 'default.template')))
-  end
-
-  def test_render
-    @cli.parse(['create', @dir])
-    reroute { Webgen::CLI::CommandParser.new.parse(['-d', @dir, 'render']) }
-    assert(File.file?(File.join(@dir, 'out', 'index.html')))
-  end
-
-  def test_all_help
-    %w[create apply render].each do |cmd|
-      soutr = reroute do
-        begin
-          @cli.parse(['help', cmd])
-        rescue SystemExit
-        end
+    def test_reroute
+      soutr = reroute do |sinw, soutr, sinr, soutw|
+        soutw.puts "test"
       end
-      assert_match(/#{cmd}:.*Usage:/m, soutr.read)
+      assert_equal("test\n", soutr.read)
     end
+
+    def teardown
+      FileUtils.rm_rf(@dir)
+    end
+
+    def test_create_simple
+      @cli.parse(['create', @dir])
+      assert(File.directory?(@dir))
+      assert(File.file?(File.join(@dir, 'config.yaml')))
+    end
+
+    def test_create_with_bundles
+      @cli.parse(['create', '-b', 'default', '-b', '07', @dir])
+      assert(File.directory?(@dir))
+      assert(File.file?(File.join(@dir, 'config.yaml')))
+      assert(File.file?(File.join(@dir, 'src', 'default.template')))
+    end
+
+    def test_create_with_no_bundle
+      @cli.parse(['create', '-b', 'none', @dir])
+      assert(File.directory?(@dir))
+      assert(File.file?(File.join(@dir, 'config.yaml')))
+      assert(!File.file?(File.join(@dir, 'src', 'default.template')))
+    end
+
+    def test_apply
+      assert_raise(RuntimeError) { @cli.parse(['-d', @dir, 'apply', '07']) }
+      @cli.parse(['create', '-b', 'none', @dir])
+      assert_raise(SystemExit) { reroute { @cli.parse(['-d', @dir, 'apply']) }}
+      reroute do |sinwrite, soutread|
+        sinwrite.puts "no"
+        @cli.parse(['-d', @dir, 'apply', '07'])
+      end
+      assert(!File.file?(File.join(@dir, 'src', 'default.template')))
+      reroute do |sinwrite, soutread|
+        sinwrite.puts "yes"
+        @cli.parse(['-d', @dir, 'apply', '07'])
+      end
+      assert(File.file?(File.join(@dir, 'src', 'default.template')))
+    end
+
+    def test_apply_forced
+      @cli.parse(['create', '-b', 'none', @dir])
+      reroute { @cli.parse(['-d', @dir, 'apply', '-f', '07']) }
+      assert(File.file?(File.join(@dir, 'src', 'default.template')))
+    end
+
+    def test_render
+      @cli.parse(['create', @dir])
+      reroute { Webgen::CLI::CommandParser.new.parse(['-d', @dir, 'render']) }
+      assert(File.file?(File.join(@dir, 'out', 'index.html')))
+    end
+
+    def test_all_help
+      %w[create apply render].each do |cmd|
+        soutr = reroute do
+          begin
+            @cli.parse(['help', cmd])
+          rescue SystemExit
+          end
+        end
+        assert_match(/#{cmd}:.*Usage:/m, soutr.read)
+      end
+    end
+
   end
 
 end
