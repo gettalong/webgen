9d1b292ba3915021328301b662cb2ea8cf684a64
Thomas Leitner <t_leitner@gmx.at>
2010-02-04 19:35:18 +0100

    Updated content processor head to allow the specification of links by the user

--- test/test_contentprocessor_head.rb
+++ test/test_contentprocessor_head.rb
@@ -10,16 +10,23 @@ class TestContentProcessorHead < Test::Unit::TestCase
 
   include Test::WebsiteHelper
 
-  def test_call
-    obj = Webgen::ContentProcessor::Head.new
-    root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/')
-    node = Webgen::Node.new(root, 'test', 'test', {'meta' => {'other' => 'me'}})
+  def setup
+    super
+    @obj = Webgen::ContentProcessor::Head.new
+    @root = Webgen::Node.new(Webgen::Tree.new.dummy_root, '/', '/')
+    @node = Webgen::Node.new(@root, 'test.en', 'test', {'lang' => 'en'})
+  end
 
-    context = Webgen::Context.new(:chain => [node])
+  def test_meta_information
+    context = Webgen::Context.new(:chain => [@node])
     context.content = '</head>'
-    obj.call(context)
+    @node['meta'] = {'other' => 'me'}
+    @obj.call(context)
     assert_equal("\n<meta name=\"other\" content=\"me\" /></head>", context.content)
+  end
 
+  def test_persistent_information
+    context = Webgen::Context.new(:chain => [@node])
     context.content = '</head>'
     context.clone.persistent[:cp_head] = {
       :js_file => ['hallo.js', 'hallo2.js', 'hallo.js'],
@@ -28,8 +35,7 @@ class TestContentProcessorHead < Test::Unit::TestCase
       :css_inline => ["somestyle", "anotherstyle"],
       :meta => {:lucky => 'me<"'}
     }
-    node['meta'].clear
-    obj.call(context)
+    @obj.call(context)
     assert_equal("\n<script type=\"text/javascript\" src=\"hallo.js\"></script>" +
                  "\n<script type=\"text/javascript\" src=\"hallo2.js\"></script>" +
                  "\n<script type=\"text/javascript\">\nsomescript\n</script>" +
@@ -39,6 +45,46 @@ class TestContentProcessorHead < Test::Unit::TestCase
                  "\n<style type=\"text/css\"><![CDATA[/\nsomestyle\n]]></style>" +
                  "\n<style type=\"text/css\"><![CDATA[/\nanotherstyle\n]]></style>" +
                  "\n<meta name=\"lucky\" content=\"me&lt;&quot;\" /></head>", context.content)
+  end
+
+  def test_links_to_other_lang_nodes
+    context = Webgen::Context.new(:chain => [@node])
+    context.content = '</head>'
+    @obj.call(context)
+    assert_equal("</head>", context.content)
+
+    de_node = Webgen::Node.new(@root, 'test.de', 'test', {'lang' => 'de'})
+    @obj.call(context)
+    assert_equal("\n<link type=\"text/html\" rel=\"alternate\" hreflang=\"de\" href=\"test.de\" /></head>", context.content)
+
+    context.content = '</head>'
+    de_node['title'] = 'Deutscher Titel'
+    @obj.call(context)
+    assert_equal("\n<link type=\"text/html\" rel=\"alternate\" hreflang=\"de\" href=\"test.de\" " +
+                 "lang=\"de\" title=\"Deutscher Titel\" /></head>", context.content)
+  end
+
+  def test_javascript_and_css_links
+    context = Webgen::Context.new(:chain => [@node])
+    context.content = '</head>'
+    @node['link'] = {'javascript' => 'http://example.org', 'css' => ['http://example.org', 'test', 'unknown']}
+    @obj.call(context)
+    assert_equal("\n<script type=\"text/javascript\" src=\"http://example.org\"></script>" +
+                 "\n<link rel=\"stylesheet\" href=\"http://example.org\" type=\"text/css\" />" +
+                 "\n<link rel=\"stylesheet\" href=\"test.en\" type=\"text/css\" /></head>", context.content)
+  end
+
+  def test_generic_links
+    context = Webgen::Context.new(:chain => [@node])
+    context.content = '</head>'
+    @node['link'] = {'next' => 'test', 'start' => {'type' => 'text/xhtml', 'href' => 'http://example.org'},
+      'alternate' => ['test', {'type' => 'text/html', 'href' => 'test'}, {'href' => 'unknown'}, {'type' => 'text/html'}]
+    }
+    @obj.call(context)
+    assert_equal("\n<link href=\"test.en\" rel=\"alternate\" />" +
+                 "\n<link href=\"test.en\" rel=\"alternate\" type=\"text/html\" />" +
+                 "\n<link href=\"test.en\" rel=\"next\" type=\"text/html\" />" +
+                 "\n<link href=\"http://example.org\" rel=\"start\" type=\"text/xhtml\" /></head>", context.content)
   end
 
 end
