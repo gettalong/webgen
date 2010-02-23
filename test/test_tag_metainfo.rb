d09c041346c177808b8c1b2ebddc669e310dab4a
Thomas Leitner <t_leitner@gmx.at>
2010-02-23 07:39:39 +0100

    Fixed bug in Tag::Metainfo where output was not correctly escaped

--- test/test_tag_metainfo.rb
+++ test/test_tag_metainfo.rb
@@ -10,11 +10,13 @@ class TestTagMetainfo < Test::Unit::TestCase
 
   def test_call
     @obj = Webgen::Tag::Metainfo.new
-    node = Webgen::Node.new(Webgen::Tree.new.dummy_root, 'hallo.page', 'hallo.page', 'test' => 10, 'lang' => 'en')
+    node = Webgen::Node.new(Webgen::Tree.new.dummy_root, 'hallo.page', 'hallo.page', 'test' => 10, 'lang' => 'en',
+                            'bad' => 'Something <&>" Bad')
     c = Webgen::Context.new(:chain => [node])
     assert_equal('', @obj.call('invalid', '', c))
     assert_equal('10', @obj.call('test', '', c))
     assert_equal('en', @obj.call('lang', '', c))
+    assert_equal('Something &lt;&amp;&gt;&quot; Bad', @obj.call('bad', '', c))
   end
 
 end
