a3c9d9eb1aff44428dadcaabe3e74913696329a3
Thomas Leitner <t_leitner@gmx.at>
2011-02-23 21:44:09 +0100

    Finished node finder API for now
    Added missing filters and sorting mechanism

--- lib/webgen/node_finder.rb
+++ lib/webgen/node_finder.rb
@@ -22,33 +22,50 @@ module Webgen
   #
   # === Non-filter options
   #
-  # [limit] Value: an integer. Specifies the maximum number of nodes that should be returned.
-  #         Implies <tt>flatten = true</tt>.
+  # [:limit] Value: an integer. Specifies the maximum number of nodes that should be returned.
+  #          Implies <tt>flatten = true</tt>.
   #
-  #         Note that fewer nodes may be returned if fewer nodes match the filter criterias.
+  #          Note that fewer nodes may be returned if fewer nodes match the filter criterias.
   #
-  # [offset] Value: an integer. Specifies how many nodes from the front of the list should not be
-  #          returned. Implies <tt>flatten = true</tt>.
+  # [:offset] Value: an integer. Specifies how many nodes from the front of the list should not be
+  #           returned. Implies <tt>flatten = true</tt>.
   #
-  # [flatten] Value: anything except +nil+ or +false+. A flat list of nodes is returned if this
-  #           option is set, otherwise the nodes are returned in their correct hierarchical order
-  #           using nested lists: If a node has no sub nodes, only the node itself is used;
-  #           otherwise a two element array containing the node and child nodes is used.
+  # [:flatten] Value: anything except +nil+ or +false+. A flat list of nodes is returned if this
+  #            option is set, otherwise the nodes are returned in their correct hierarchical order
+  #            using nested lists: If a node has no sub nodes, only the node itself is used;
+  #            otherwise a two element array containing the node and child nodes is used.
   #
-  #           Note that any missing nodes in the hierarchy are automatically added so that
-  #           traversing the hierarchy is always possible. For example, if we have the tree
-  #           <tt>/a/b/c</tt> and only nodes +a+ and +c+ are found, node +b+ is automatically added.
+  #            Note that any missing nodes in the hierarchy are automatically added so that
+  #            traversing the hierarchy is always possible. For example, if we have the tree
+  #            <tt>/a/b/c</tt> and only nodes +a+ and +c+ are found, node +b+ is automatically
+  #            added.
   #
-  # [sort] TODO
+  # [:sort] Value: +nil+/+false+, +true+ or a meta information key. If +nil+ or +false+ is
+  #         specified, no sorting is performed. If +true+ is specified, the meta information
+  #         +sort_info+ (or if absent, the meta information +title+) is used for sorting. If the
+  #         compared values are both integers, a numeric comparison is done, else a string
+  #         comparison. If a meta information key is specified, the value of this meta information
+  #         is used for comparison of nodes.
   #
   # === Filter options
   #
-  # [alcn] Value: an alcn pattern or an array of alcn patterns. Nodes that match any of the patterns
-  #        are used.
+  # [:alcn] Value: an alcn pattern or an array of alcn patterns. Nodes that match any of the patterns
+  #         are used.
   #
-  # [levels] Value; one integer (is used as start and end level) or an array with two integers (the
-  #          start and end levels). All nodes whose hierarchy levels are greater than or equal to
-  #          the start level and lower than or equal to the end level are used.
+  # [:and] Value: a finder option set or an array of finder options sets (specifying option set
+  #        names is also possible). Only nodes that appear in all specified option sets are used.
+  #
+  # [:lang] Value: a language code/+nil+/the special value :+node+ or an array of these values.
+  #         Nodes that have one of the specified language codes, are language independent (in case
+  #         of the value +nil+) or have the same language as the reference node (in case of the
+  #         value :+node+) are used.
+  #
+  # [:levels] Value: one integer (is used as start and end level) or an array with two integers (the
+  #           start and end levels). All nodes whose hierarchy levels are greater than or equal to
+  #           the start level and lower than or equal to the end level are used.
+  #
+  # [:or] Value: a finder option set or an array of finder options sets (specifying option set names
+  #       is also possible). Nodes that appear in any specified option set are additionally used.
   #
   # == Implementing a filter module
   #
@@ -79,7 +96,10 @@ module Webgen
 
     def initialize # :nodoc:
       super
-      @mapping = {'alcn' => :filter_alcn, 'levels' => :filter_levels}
+      @mapping = {
+        :alcn => :filter_alcn, :levels => :filter_levels, :lang => :filter_lang,
+        :and => :filter_and, :or => :filter_or
+      }
     end
 
     # Add a module with filter methods. The parameter +mapping+ needs to be a hash associating
@@ -95,7 +115,7 @@ module Webgen
         if !public_methods.include?(method.to_s)
           raise ArgumentError, "Finder method '#{method}' not found in module #{mod}"
         end
-        @mapping[name.to_s] = method
+        @mapping[name.intern] = method
       end
       extend(mod)
     end
@@ -104,39 +124,18 @@ module Webgen
     # hash with finder options or the name of a finder option set defined using the configuration
     # option <tt>node_finder.options_sets</tt>. The node +ref_node+ is used as reference node.
     def find(opts_or_name, ref_node)
-      opts, name = if Hash === opts_or_name
-                     [opts_or_name.dup, opts_or_name['name']]
-                   elsif website.config['node_finder.option_sets'].has_key?(opts_or_name)
-                     [website.config['node_finder.option_sets'][opts_or_name].dup, opts_or_name]
-                   else
-                     raise ArgumentError, "Invalid argument supplied, expected Hash or name of search definition, not #{opts_or_name}"
-                   end
-      raise ArgumentError, "Each node finder option set needs a name" if name.to_s.empty?
+      opts = prepare_options_hash(opts_or_name)
+      raise ArgumentError, "Each node finder option set needs a name" if opts[:name].to_s.empty?
 
-      limit = opts.delete('limit')
-      offset = opts.delete('offset')
-      flatten = opts.delete('flatten')
-      sort = opts.delete('sort')
+      limit, offset, flatten, sort = remove_non_filter_options(opts)
+      flatten = true if limit || offset
 
-      nodes = website.tree.node_access[:alcn].values
-      opts.keys.each do |filter|
-        filter = filter.to_s
-        if @mapping.has_key?(filter)
-          nodes = send(@mapping[filter], nodes, ref_node, opts[filter])
-        else
-          website.logger.warn { "Ignorning unknown filter '#{filter}' for node finder option set '#{name}'" }
-        end
-      end
+      nodes = filter_nodes(opts, ref_node)
 
-      #TODO: result.sort!(opts[:sort]) if result
-      #sort needs to be done before limit/offset if flatten, else AFTER hierarchy is built
-
-      if limit || offset
-        nodes = nodes[(offset.to_s.to_i)..(limit ? offset.to_s.to_i + limit.to_s.to_i - 1 : -1)]
-        flatten = true
-      end
-
-      if !flatten
+      if flatten
+        sort_nodes(nodes, sort)
+        nodes = nodes[(offset.to_s.to_i)..(limit ? offset.to_s.to_i + limit.to_s.to_i - 1 : -1)] if limit || offset
+      else
         result = {}
         min_level = Float::INFINITY
         nodes.each {|n| min_level = n.level if n.level < min_level}
@@ -151,6 +150,7 @@ module Webgen
           h.map {|k,v| v.empty? ? k : [k, reducer.call(v)]}
         end
         nodes = reducer.call(result)
+        sort_nodes(nodes, sort, false)
       end
 
       nodes
@@ -160,14 +160,82 @@ module Webgen
     private
     #######
 
+    def prepare_options_hash(opts_or_name)
+      if Hash === opts_or_name
+        opts_or_name.dup
+      elsif website.config['node_finder.option_sets'].has_key?(opts_or_name)
+        website.config['node_finder.option_sets'][opts_or_name].dup
+      else
+        raise ArgumentError, "Invalid argument supplied, expected Hash or name of search definition, not #{opts_or_name}"
+      end
+    end
+
+    def remove_non_filter_options(opts)
+      opts.delete(:name)
+      [opts.delete(:limit), opts.delete(:offset), opts.delete(:flatten), opts.delete(:sort)]
+    end
+
+    def filter_nodes(opts, ref_node)
+      nodes = website.tree.node_access[:alcn].values
+      nodes.delete(website.tree.dummy_root)
+
+      opts.delete_if do |filter, value|
+        if @mapping.has_key?(filter)
+          nodes = send(@mapping[filter], nodes, ref_node, value)
+        elsif filter.kind_of?(Symbol)
+          website.logger.warn { "Ignorning unknown node finder filter '#{filter}'" }
+        end
+        !filter.kind_of?(String)
+      end
+
+      nodes = filter_meta_info(nodes, ref_node, opts) unless opts.empty?
+      nodes
+    end
+
+    def sort_nodes(nodes, sort, flat_mode = true)
+      return unless sort
+      if sort == true
+        nodes.sort! do |(a,_),(b,_)|
+          a = (a['sort_info'] && a['sort_info'].to_s) || a['title'] || ''
+          b = (b['sort_info'] && b['sort_info'].to_s) || b['title'] || ''
+          (a = a.to_i; b = b.to_i) if a !~ /\D/ && b !~ /\D/
+          a <=> b
+        end
+      else
+        nodes.sort! {|(a,_),(b,_)| a[sort] <=> b[sort]}
+      end
+      nodes.each {|n, children| sort_nodes(children, sort) if children } unless flat_mode
+    end
+
+    # :section: Filter methods
+
+    def filter_and(nodes, ref_node, opts)
+      [opts].flatten.each {|cur_opts| nodes &= filter_nodes(prepare_options_hash(cur_opts), ref_node)}
+      nodes
+    end
+
+    def filter_or(nodes, ref_node, opts)
+      [opts].flatten.each {|cur_opts| nodes |= filter_nodes(prepare_options_hash(cur_opts), ref_node)}
+      nodes
+    end
+
+    def filter_meta_info(nodes, ref_node, mi)
+      nodes.keep_if {|n| mi.all? {|key, val| n[key] == val}}
+    end
+
     def filter_alcn(nodes, ref_node, alcn)
-      alcn = [alcn].flatten.map {|a| Webgen::Common.append_path(ref_node.alcn, a)}
-      nodes.select {|n| alcn.any? {|a| n =~ a}}
+      alcn = [alcn].flatten.map {|a| Webgen::Common.append_path(ref_node.alcn, a.to_s)}
+      nodes.keep_if {|n| alcn.any? {|a| n =~ a}}
     end
 
     def filter_levels(nodes, ref_node, range)
-      range = [range].flatten
-      nodes.select {|n| n.level >= range.first && n.level <= range.last}
+      range = [range].flatten.map {|i| i.to_i}
+      nodes.keep_if {|n| n.level >= range.first && n.level <= range.last}
+    end
+
+    def filter_lang(nodes, ref_node, langs)
+      langs = [langs].flatten.map {|l| l == :node ? ref_node.lang : l}.uniq
+      nodes.keep_if {|n| langs.any? {|l| n.lang == l}}
     end
 
   end
--- test/webgen/test_node_finder.rb
+++ test/webgen/test_node_finder.rb
@@ -23,7 +23,7 @@ class TestNodeFinder < MiniTest::Unit::TestCase
     assert_raises(ArgumentError) { @nf.add_filter_module(m, :name => 'unknown_method') }
     assert_raises(NoMethodError) { @nf.filter_something }
     @nf.add_filter_module(m, :something => 'filter_something')
-    assert_equal('filter_something', @nf.instance_eval { @mapping['something'] })
+    assert_equal('filter_something', @nf.instance_eval { @mapping[:something] })
     assert_equal(:value, @nf.filter_something)
   end
 
@@ -31,11 +31,14 @@ class TestNodeFinder < MiniTest::Unit::TestCase
     tree = Webgen::Tree.new(@nf.website)
     @nf.website.expect(:tree, tree)
     @nf.website.expect(:logger, ::Logger.new(StringIO.new))
+    blackboard = MiniTest::Mock.new
+    blackboard.expect(:dispatch_msg, nil, [nil, nil, nil])
+    @nf.website.expect(:blackboard, blackboard)
 
     nodes = {
       :root => root = Webgen::Node.new(tree.dummy_root, '/', '/'),
-      :somename_en => child_en = Webgen::Node.new(root, 'somename.html', '/somename.en.html', {'lang' => 'en', 'title' => 'somename en'}),
-      :somename_de => child_de = Webgen::Node.new(root, 'somename.html', '/somename.de.html', {'lang' => 'de', 'title' => 'somename de'}),
+      :somename_en => child_en = Webgen::Node.new(root, 'somename.html', '/somename.en.html', {'lang' => 'en', 'title' => 'somename en', 'sort_info' => 3}),
+      :somename_de => child_de = Webgen::Node.new(root, 'somename.html', '/somename.de.html', {'lang' => 'de', 'title' => 'somename de', 'sort_info' => 5}),
       :other => Webgen::Node.new(root, 'other.html', '/other.html', {'title' => 'other'}),
       :other_en => Webgen::Node.new(root, 'other.html', '/other1.html', {'lang' => 'en', 'title' => 'other en'}),
       :somename_en_frag => frag_en = Webgen::Node.new(child_en, '#othertest', '/somename.en.html#frag', {'title' => 'frag'}),
@@ -44,7 +47,7 @@ class TestNodeFinder < MiniTest::Unit::TestCase
       :dir_file => dir_file = Webgen::Node.new(dir, 'file.html', '/dir/file.html', {'title' => 'file'}),
       :dir_file_frag => Webgen::Node.new(dir_file, '#frag', '/dir/file.html#frag', {'title' => 'frag'}),
       :dir_dir => dir_dir = Webgen::Node.new(dir, 'subdir/' , '/dir/subdir/', {'title' => 'dir'}),
-      :dir_dir_file => Webgen::Node.new(dir_dir, 'file.html', '/dir/subdir/file.html', {'title' => 'file'}),
+      :dir_dir_file => Webgen::Node.new(dir_dir, 'file.html', '/dir/subdir/file.html', {'title' => 'file1'}),
       :dir2 => dir2 = Webgen::Node.new(root, 'dir2/', '/dir2/', {'proxy_path' => 'index.html', 'title' => 'dir2'}),
       :dir2_index_en => Webgen::Node.new(dir2, 'index.html', '/dir2/index.html',
                                          {'lang' => 'en', 'routed_title' => 'routed', 'title' => 'index en'}),
@@ -52,42 +55,80 @@ class TestNodeFinder < MiniTest::Unit::TestCase
                                          {'lang' => 'de', 'routed_title' => 'routed_de', 'title' => 'index de'}),
     }
 
-    assert_raises(ArgumentError) { @nf.find({'alcn' => '/'}, nodes[:root]) }
+    check = lambda do |correct, result|
+      assert_equal(correct.collect {|n| nodes[n] }, result, "Failure at #{caller[0]}")
+    end
+
+    assert_raises(ArgumentError) { @nf.find({:alcn => '/'}, nodes[:root]) }
 
     # test using configured search options
-    @config['node_finder.option_sets'] = {'simple' => {'alcn' => ''}}
-    assert_equal([nodes[:root]], @nf.find('simple', nodes[:root]))
-    assert_equal([nodes[:somename_en]], @nf.find('simple', nodes[:somename_en]))
+    @config['node_finder.option_sets'] = {'simple' => {:alcn => '', :unknown => '', :name => 'simple'}}
+    check.call([:root], @nf.find('simple', nodes[:root]))
+    check.call([:somename_en], @nf.find('simple', nodes[:somename_en]))
 
     # test limit, offset, flatten
-    assert_equal([:somename_en, :other_en].map {|n| nodes[n]},
-                 @nf.find({'alcn' => '/**/*.en.html', 'limit' => 2, 'name' => 'test'}, nodes[:root]))
-    assert_equal([:dir2_index_en].map {|n| nodes[n]},
-                 @nf.find({'alcn' => '/**/*.en.html', 'limit' => 2, 'offset' => 2, 'name' => 'test'}, nodes[:root]))
+    check.call([:somename_en, :other_en],
+               @nf.find({:alcn => '/**/*.en.html', :limit => 2, :name => 'test'}, nodes[:root]))
+    check.call([:dir2_index_en],
+               @nf.find({:alcn => '/**/*.en.html', :limit => 2, :offset => 2, :name => 'test'}, nodes[:root]))
 
     assert_equal([[nodes[:somename_en], [nodes[:somename_en_frag]]], nodes[:somename_de],
                   nodes[:other], nodes[:other_en],
                   [nodes[:dir], [nodes[:dir_file], nodes[:dir_dir]]],
                   [nodes[:dir2], [nodes[:dir2_index_en], nodes[:dir2_index_de]]]
                  ],
-                 @nf.find({'levels' => [1,2], 'name' => 'test'}, nodes[:root]))
+                 @nf.find({:levels => [1,2], :name => 'test'}, nodes[:root]))
+
+    # test sort methods
+    check.call([:somename_en, :somename_de, :dir_file, :dir_dir_file, :dir2_index_de,
+                :dir2_index_en, :other, :other_en],
+               @nf.find({:alcn => '/**/*.html', :name => 'test', :flatten => true, :sort => true}, nodes[:root]))
+    check.call([:dir_file, :dir_dir_file, :dir2_index_de, :dir2_index_en, :other, :other_en,
+                :somename_de, :somename_en],
+               @nf.find({:alcn => '/**/*.html', :name => 'test', :flatten => true, :sort => 'title'}, nodes[:root]))
+    assert_equal([[nodes[:somename_en], [nodes[:somename_en_frag]]], nodes[:somename_de],
+                  [nodes[:dir], [nodes[:dir_dir], nodes[:dir_file]]],
+                  [nodes[:dir2], [nodes[:dir2_index_de], nodes[:dir2_index_en]]],
+                  nodes[:other], nodes[:other_en],
+                 ],
+                 @nf.find({:levels => [1,2], :name => 'test', :sort => true}, nodes[:root]))
+
+    # test filter: meta info keys/values
+    check.call([:somename_en_frag, :dir_file_frag],
+               @nf.find({'title' => 'frag', :name => 'test', :flatten => true}, nodes[:root]))
 
     # test filter: alcn
-    assert_equal([nodes[:root]],
-                 @nf.find({'alcn' => '/', 'name' => 'test'}, nodes[:root]))
-    assert_equal([:somename_en, :somename_de, :other, :other_en, :dir_file, :dir_dir_file,
-                  :dir2_index_en, :dir2_index_de].map {|n| nodes[n]},
-                 @nf.find({'alcn' => '/**/*.html', 'name' => 'test', 'flatten' => true}, nodes[:root]))
-    assert_equal([:root, :dir_file, :dir_file_frag, :dir_dir].map {|n| nodes[n]},
-                 @nf.find({'alcn' => ['/', '*'], 'name' => 'test', 'flatten' => true}, nodes[:dir]))
+    check.call([:root],
+               @nf.find({:alcn => '/', :name => 'test'}, nodes[:root]))
+    check.call([:somename_en, :somename_de, :other, :other_en, :dir_file, :dir_dir_file,
+                :dir2_index_en, :dir2_index_de],
+               @nf.find({:alcn => '/**/*.html', :name => 'test', :flatten => true}, nodes[:root]))
+    check.call([:root, :dir_file, :dir_file_frag, :dir_dir],
+               @nf.find({:alcn => ['/', '*'], :name => 'test', :flatten => true}, nodes[:dir]))
+
+    # test filter: and/or
+    check.call([:somename_en, :somename_de, :other, :other_en],
+               @nf.find({:alcn => '/**/*.html', :and => {:alcn => '*.html'},
+                        :name => 'test', :flatten => true}, nodes[:root]))
+    check.call([:somename_en, :somename_de, :other, :other_en, :dir_file, :dir_dir_file,
+                :dir2_index_en, :dir2_index_de, :root],
+               @nf.find({:alcn => '/**/*.html', :or => 'simple', :name => 'test', :flatten => true}, nodes[:root]))
 
     # test filter: levels
-    assert_equal([nodes[:root]],
-                 @nf.find({'levels' => [0, 0], 'name' => 'test', 'flatten' => true}, nodes[:dir]))
-    assert_equal([:somename_en_fragnest, :dir_file_frag, :dir_dir_file].map {|n| nodes[n]},
-                 @nf.find({'levels' => [3,3], 'name' => 'test', 'flatten' => true}, nodes[:dir]))
-    assert_equal([:root, :somename_en, :somename_de, :other, :other_en, :dir, :dir2].map {|n| nodes[n]},
-                 @nf.find({'levels' => [0,1], 'name' => 'test', 'flatten' => true}, nodes[:dir]))
+    check.call([:root],
+               @nf.find({:levels => [0, 0], :name => 'test', :flatten => true}, nodes[:dir]))
+    check.call([:somename_en_fragnest, :dir_file_frag, :dir_dir_file],
+               @nf.find({:levels => [3,3], :name => 'test', :flatten => true}, nodes[:dir]))
+    check.call([:root, :somename_en, :somename_de, :other, :other_en, :dir, :dir2],
+               @nf.find({:levels => [0,1], :name => 'test', :flatten => true}, nodes[:dir]))
+
+    # test filter: langs
+    check.call([:somename_en, :other_en, :dir2_index_en],
+               @nf.find({:lang => 'en', :name => 'test', :flatten => true}, nodes[:dir]))
+    check.call([:somename_en, :somename_de, :other_en, :dir2_index_en, :dir2_index_de],
+               @nf.find({:lang => ['en', 'de'], :name => 'test', :flatten => true}, nodes[:dir]))
+    check.call([:somename_en, :other_en, :dir2_index_en],
+               @nf.find({:lang => :node, :name => 'test', :flatten => true}, nodes[:somename_en]))
 
     @nf.website.verify
   end
