1e33e8bc70dabf2173c10193dd5f6466215d86f2
Thomas Leitner <t_leitner@gmx.at>
2011-10-30 09:48:48 +0100

    Updated content processor Sass

--- lib/webgen/content_processor/sass.rb
+++ lib/webgen/content_processor/sass.rb
@@ -1,16 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 require 'webgen/content_processor'
-webgen_require 'sass', 'haml'
+webgen_require 'sass'
 
 module Webgen
   class ContentProcessor
 
-    # Processes content in Sass markup (used for writing CSS files) using the +haml+ library.
-    class Sass
+    # Processes content in Sass markup (used for writing CSS files).
+    module Sass
 
       # Convert the content in +sass+ markup to CSS.
-      def call(context)
+      def self.call(context)
         context.content = ::Sass::Engine.new(context.content, :filename => context.ref_node.alcn).render
         context
       rescue ::Sass::SyntaxError => e
