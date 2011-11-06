6a2242e284ba442899258f4637da5833b5dfd2c3
Thomas Leitner <t_leitner@gmx.at>
2011-11-06 21:16:27 +0100

    Extraced the functionality of tag parsing into a separate class

---
+++ lib/webgen/utils/tag_parser.rb
@@ -0,0 +1,123 @@
+# -*- encoding: utf-8 -*-
+
+require 'yaml'
+require 'strscan'
+require 'webgen/error'
+
+module Webgen
+  module Utils
+
+    # This class is used to parse a string for webgen tags and replace them with dynamically
+    # generated content. See #replace_tags for more information.
+    class TagParser
+
+      # Raised by the Webgen::Utils::TagParser class.
+      class Error < Webgen::Error
+
+        attr_accessor :line
+        attr_accessor :column
+
+      end
+
+
+      # Create a new TagParser object, optionally providing a tag prefix.
+      def initialize(prefix = nil)
+        @start_re = /(\\*)\{#{prefix}(\w+)(::?)/
+        @end_re = /(\\*)\{#{prefix}(\w+)\}/
+      end
+
+      BRACKETS_RE = /([{}])/ # :nodoc:
+      ProcessingStruct = Struct.new(:state, :tag, :simple_tag, :backslashes, :brackets, :start_pos, :end_pos,
+                                    :params_start_pos, :params_end_pos, :body_end_pos) # :nodoc:
+
+      # Return the +content+ with all webgen tags replaced. When a webgen tag is encountered by the
+      # parser, the method yields all found information and substitutes the returned string for the
+      # tag.
+      def replace_tags(content) #:yields: tag_name, params, body
+        scanner = StringScanner.new(content)
+        data = ProcessingStruct.new(:before_tag)
+        while true
+          case data.state
+          when :before_tag
+            if scanner.skip_until(@start_re)
+              data.state = :in_start_tag
+              data.backslashes = scanner[1].length
+              data.brackets = 1
+              data.tag = scanner[2]
+              data.simple_tag = (scanner[3] == ':')
+              data.params_start_pos = scanner.pos
+              data.start_pos = scanner.pos - scanner.matched.length
+            else
+              data.state = :done
+            end
+
+          when :in_start_tag
+            data.brackets += (scanner[1] == '{' ? 1 : -1) while data.brackets != 0 && scanner.skip_until(BRACKETS_RE)
+            if data.brackets != 0
+              raise Error.new("Unbalanced curly brackets found for tag '#{data.tag}'")
+            else
+              data.params_end_pos = data.body_end_pos = data.end_pos = scanner.pos - 1
+              data.state = (data.simple_tag ? :process : :in_body)
+            end
+
+          when :process
+            begin
+              enc = scanner.string.encoding
+              scanner.string.force_encoding('ASCII-8BIT')
+              if data.backslashes % 2 == 0
+                params = parse_params(scanner.string[data.params_start_pos...data.params_end_pos].force_encoding(enc),
+                                      data.tag)
+                result = yield(data.tag, params,
+                               scanner.string[(data.params_end_pos+1)...data.body_end_pos].to_s.force_encoding(enc))
+                result = result.to_s.force_encoding('ASCII-8BIT')
+                scanner.string[data.start_pos..data.end_pos] = "\\" * (data.backslashes / 2) + result
+                scanner.pos = data.start_pos + result.length
+              else
+                scanner.string[data.start_pos, 1 + data.backslashes / 2] = ''
+                scanner.pos -= 1 + data.backslashes / 2
+              end
+            ensure
+              scanner.string.force_encoding(enc)
+            end
+            data.state = :before_tag
+
+          when :in_body
+            while (result = scanner.skip_until(@end_re))
+              next unless scanner[2] == data.tag
+              if scanner[1].length % 2 == 1
+                scanner.string[(scanner.pos - scanner.matched.length), 1 + scanner[1].length / 2] = ''
+                scanner.pos -= 1 + scanner[1].length / 2
+              else
+                break
+              end
+            end
+            if result
+              data.state = :process
+              data.end_pos = scanner.pos - 1
+              data.body_end_pos = scanner.pos - scanner.matched.length + scanner[1].length / 2
+            else
+              raise Error.new("Invalid body part - no end tag found for '#{data.tag}'")
+            end
+
+          when :done
+            break
+          end
+        end
+        scanner.string
+      rescue Error => e
+        e.line = scanner.string[0...data.start_pos].scan("\n").size + 1
+        e.column = data.start_pos - (scanner.string.rindex("\n", data.start_pos) || -1)
+        raise
+      end
+
+      # Parse the parameter string.
+      def parse_params(param_string, tag)
+        YAML::load("--- #{param_string}")
+      rescue ArgumentError => e
+        raise Error.new("Could not parse parameter string '#{param_string}' for tag '#{tag}': #{e.message}")
+      end
+
+    end
+
+  end
+end
---
+++ test/webgen/utils/test_tag_parser.rb
@@ -0,0 +1,99 @@
+# -*- encoding: utf-8 -*-
+
+require 'helper'
+require 'webgen/utils/tag_parser'
+
+class TestTagParser < MiniTest::Unit::TestCase
+
+  include Test::WebgenAssertions
+
+  def test_replace_tags
+    @parser = Webgen::Utils::TagParser.new
+
+    # no webgen tag in content
+    check_returned_tags('sdfsdf{asd', [])
+    check_returned_tags('sdfsdf}asd', [])
+    check_returned_tags('sdfsdf{asd}', [])
+
+    # error with invalid brace nesting
+    check_returned_tags('sdfsdf{asd: {}as', [], true)
+    check_returned_tags('sdfsdf{test: {test1: {}}', [], true)
+
+    # error with invalid params
+    check_returned_tags('sdfsdf{test: {test1: [}}', [], true)
+
+    # normal behavious
+    check_returned_tags('sdfsdf{test: {test1: }}', [['test', {'test1' => nil}, '']], 'sdfsdftest1')
+    check_returned_tags('sdfsdf{test:}{test1: }', [['test', nil, ''], ['test1', nil, '']], 'sdfsdftest1test2')
+
+    # brace escaping
+    check_returned_tags('sdfsdf{test:}\\{test1: }', [['test', nil, '']], "sdfsdftest1{test1: }")
+    check_returned_tags('sdfsdf\\{test:}{test1:}', [['test1', nil, '']], "sdfsdf{test:}test1")
+    check_returned_tags('sdfsdf{test: asdf}', [['test', 'asdf', '']], "sdfsdftest1")
+    check_returned_tags('sdfsdf\\{test: asdf}', [], "sdfsdf{test: asdf}")
+    check_returned_tags('sdfsdf\\\\{test: asdf}', [['test', 'asdf', '']], "sdfsdf\\test1")
+    check_returned_tags('sdfsdf\\\\\\{test: asdf}sdf', [['test', 'asdf', '']], "sdfsdf\\{test: asdf}sdf")
+
+    # error when no end tag is found
+    check_returned_tags('before{test::}body{testno}', [], true)
+
+    # tags with body
+    check_returned_tags('before{test::}body{test}', [['test', nil, 'body']], "beforetest1")
+    check_returned_tags('before{test::}body\\{test}other{test}', [['test', nil, 'body{test}other']], "beforetest1")
+    check_returned_tags('before{test::}body\\{test}{test}', [['test', nil, 'body{test}']], "beforetest1")
+    check_returned_tags('before{test::}body\\{test}\\\\{test}after', [['test', nil, 'body{test}\\']], "beforetest1after")
+    check_returned_tags('before\\{test::}body{test}', [['test', nil, 'body']], "before{test::}body{test}")
+    check_returned_tags('before\\\\{test:: asdf}body{test}after', [['test', 'asdf', 'body']], "before\\test1after")
+
+    # content with non-ascii characters
+    check_returned_tags('sdfsüdf{test: }', [['test', nil, '']], "sdfsüdftest1")
+    check_returned_tags('sdfsüüdf{test: asdf}', [['test', 'asdf', '']], "sdfsüüdftest1")
+    check_returned_tags('sdfsüüdf{test: asüdf}a', [['test', 'asüdf', '']], "sdfsüüdftest1a")
+    check_returned_tags('sdfsüüdf\\{test: asdf}ab', [], "sdfsüüdf{test: asdf}ab")
+    check_returned_tags('sdfsüüdf\\\\{test: asdf}ab', [['test', 'asdf', '']], "sdfsüüdf\\test1ab")
+    check_returned_tags('sdfsüüdf\\\\\\{test: asdf}ab', [], "sdfsüüdf\\{test: asdf}ab")
+    check_returned_tags('sdfsüüdf{test:: asdf}abü{test}ab', [['test', 'asdf', 'abü']], "sdfsüüdftest1ab")
+  end
+
+  def test_replace_tags_with_prefix
+    @parser = Webgen::Utils::TagParser.new('hallo')
+    check_returned_tags('sdfsüdf{test: }', [], "sdfsüdf{test: }")
+    check_returned_tags('sdfsüdf{hallotest: }', [['test', nil, '']], "sdfsüdftest1")
+  end
+
+  def check_returned_tags(content, data, result = content)
+    i = 0
+    check_proc = proc do |tag, params, body|
+      assert_equal(data[i][0], tag, 'error on tag with content: ' + content)
+      assert_equal(data[i][1], params, 'error on params with content: ' + content)
+      assert_equal(data[i][2], body, 'error on body with content: ' + content)
+      i += 1
+      'test' + i.to_s
+    end
+
+    if result.kind_of?(TrueClass)
+      assert_error_on_line(Webgen::Utils::TagParser::Error, 1) { @parser.replace_tags(content, &check_proc) }
+    else
+      @parser.replace_tags(content, &check_proc)
+      assert_equal(content, result)
+    end
+    assert(i, data.length)
+  end
+
+  def test_error_line_and_column
+    @parser = Webgen::Utils::TagParser.new
+    check_line_and_column("error\non\nline 3 {test1: {}", 3, 8)
+    check_line_and_column("line {test1: {}", 1, 6)
+    check_line_and_column("error\no\n{test1: {}", 3, 1)
+  end
+
+  def check_line_and_column(content, line, column)
+    begin
+      @parser.replace_tags(content) { }
+    rescue Webgen::Utils::TagParser::Error => e
+      assert_equal(line, e.line)
+      assert_equal(column, e.column)
+    end
+  end
+
+end
