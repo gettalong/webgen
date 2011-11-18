406ad473fa94b71fab2d567f718bb5cb2282a60b
Thomas Leitner <t_leitner@gmx.at>
2011-11-18 11:31:27 +0100

    Updated content processor Tags

--- lib/webgen/content_processor/tags.rb
+++ lib/webgen/content_processor/tags.rb
@@ -1,9 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-require 'yaml'
-require 'strscan'
 require 'webgen/content_processor'
-require 'webgen/tag'
 
 module Webgen
   class ContentProcessor
@@ -12,128 +9,17 @@ module Webgen
     #
     # webgen tags are an easy way to add dynamically generated content to websites, for example menus
     # or breadcrumb trails.
-    class Tags
-
-      include Webgen::WebsiteAccess
-      include Webgen::Loggable
-
-      def initialize #:nodoc:
-        @start_re = /(\\*)\{#{website.config['contentprocessor.tags.prefix']}(\w+)(::?)/
-        @end_re = /(\\*)\{#{website.config['contentprocessor.tags.prefix']}(\w+)\}/
-      end
+    module Tags
 
       # Replace all webgen tags in the content of +context+ with the rendered content.
-      def call(context)
-        replace_tags(context) do |tag, param_string, body|
-          params = nil
-          begin
-            params = YAML::load("--- #{param_string}")
-          rescue ArgumentError => e
-            raise Webgen::RenderError.new("Could not parse parameter string '#{param_string}' for tag '#{tag}': #{e.message}",
-                                          self.class.name, context.dest_node, context.ref_node)
+      def self.call(context)
+        context.website.ext.tag.replace_tags(context.content) do |tag, params, body|
+          context.website.logger.debug do
+            "Replacing tag #{tag} with data #{params.inspect} and body '#{body}' in <#{context.ref_node}>"
           end
-          log(:debug) { "Replacing tag #{tag} with data '#{param_string}' and body '#{body}' in <#{context.ref_node}>" }
           context.website.ext.tag.call(tag, params, body, context)
         end
         context
-      end
-
-      #######
-      private
-      #######
-
-      BRACKETS_RE = /([{}])/
-      ProcessingStruct = Struct.new(:state, :tag, :simple_tag, :backslashes, :brackets, :start_pos, :end_pos,
-                                    :params_start_pos, :params_end_pos, :body_end_pos)
-
-      # Return the <tt>context.content</tt> provided by <tt>context.ref_node</tt> with all webgen tags
-      # replaced. When a webgen tag is encountered by the parser, the method yields all found
-      # information and substitutes the returned string for the tag.
-      def replace_tags(context) #:yields: tag_name, param_string, body
-        scanner = StringScanner.new(context.content)
-        data = ProcessingStruct.new(:before_tag)
-        while true
-          case data.state
-          when :before_tag
-            if scanner.skip_until(@start_re)
-              data.state = :in_start_tag
-              data.backslashes = scanner[1].length
-              data.brackets = 1
-              data.tag = scanner[2]
-              data.simple_tag = (scanner[3] == ':')
-              data.params_start_pos = scanner.pos
-              data.start_pos = scanner.pos - scanner.matched.length
-            else
-              data.state = :done
-            end
-
-          when :in_start_tag
-            data.brackets += (scanner[1] == '{' ? 1 : -1) while data.brackets != 0 && scanner.skip_until(BRACKETS_RE)
-            if data.brackets != 0
-              raise Webgen::RenderError.new("Unbalanced curly brackets for tag '#{data.tag}'", self.class.name,
-                                            context.dest_node, context.ref_node)
-            else
-              data.params_end_pos = data.body_end_pos = data.end_pos = scanner.pos - 1
-              data.state = (data.simple_tag ? :process : :in_body)
-            end
-
-          when :process
-            if RUBY_VERSION >= '1.9'
-              begin
-                enc = scanner.string.encoding
-                scanner.string.force_encoding('ASCII-8BIT')
-                if data.backslashes % 2 == 0
-                  result = yield(data.tag, scanner.string[data.params_start_pos...data.params_end_pos].force_encoding(enc),
-                                 scanner.string[(data.params_end_pos+1)...data.body_end_pos].to_s.force_encoding(enc)).to_s
-                  scanner.string[data.start_pos..data.end_pos] = "\\" * (data.backslashes / 2) + result.force_encoding('ASCII-8BIT')
-                  scanner.pos = data.start_pos + result.length
-                else
-                  scanner.string[data.start_pos, 1 + data.backslashes / 2] = ''
-                  scanner.pos -= 1 + data.backslashes / 2
-                end
-              ensure
-                scanner.string.force_encoding(enc) if RUBY_VERSION >= '1.9'
-              end
-            else
-              if data.backslashes % 2 == 0
-                result = yield(data.tag, scanner.string[data.params_start_pos...data.params_end_pos],
-                               scanner.string[(data.params_end_pos+1)...data.body_end_pos]).to_s
-                scanner.string[data.start_pos..data.end_pos] = "\\" * (data.backslashes / 2) + result
-                scanner.pos = data.start_pos + result.length
-              else
-                scanner.string[data.start_pos, 1 + data.backslashes / 2] = ''
-                scanner.pos -= 1 + data.backslashes / 2
-              end
-            end
-            data.state = :before_tag
-
-          when :in_body
-            while (result = scanner.skip_until(@end_re))
-              next unless scanner[2] == data.tag
-              if scanner[1].length % 2 == 1
-                scanner.string[(scanner.pos - scanner.matched.length), 1 + scanner[1].length / 2] = ''
-                scanner.pos -= 1 + scanner[1].length / 2
-              else
-                break
-              end
-            end
-            if result
-              data.state = :process
-              data.end_pos = scanner.pos - 1
-              data.body_end_pos = scanner.pos - scanner.matched.length + scanner[1].length / 2
-            else
-              raise Webgen::RenderError.new("Invalid body part - no end tag found for '#{data.tag}'", self.class.name,
-                                            context.dest_node, context.ref_node)
-            end
-
-          when :done
-            break
-          end
-        end
-        scanner.string
-      rescue Webgen::RenderError => e
-        e.line = scanner.string[0...scanner.pos].scan("\n").size + 1 unless e.line
-        raise
       end
 
     end
---
+++ test/webgen/content_processor/test_tags.rb
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+
+require 'helper'
+require 'stringio'
+require 'logger'
+require 'webgen/content_processor/tags'
+require 'webgen/utils/tag_parser'
+
+class TestContentProcessorTags < MiniTest::Unit::TestCase
+
+  class SimpleTag
+
+    def set_block(&block)
+      @block = block
+    end
+
+    def call(tag, params, body, context)
+      @block.call(tag, params, body, context)
+    end
+
+    def replace_tags(content, &block)
+      Webgen::Utils::TagParser.new.replace_tags(content, &block)
+    end
+
+  end
+
+  def test_static_call
+    website, node, context = Test.setup_content_processor_test
+    context.content = '{test: }'
+    stag = SimpleTag.new
+    ext = MiniTest::Mock.new
+    ext.expect(:tag, stag)
+    website.expect(:ext, ext)
+    website.expect(:logger, Logger.new(StringIO.new))
+    cp = Webgen::ContentProcessor::Tags
+
+    stag.set_block do |tag, params, body, local_context|
+      assert_equal('test', tag)
+      assert_equal(nil, params)
+      assert_equal('', body)
+      assert_equal(context, local_context)
+    end
+    cp.call(context)
+
+    ext.verify
+    website.verify
+  end
+
+end
