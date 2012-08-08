5c3b4c27ce121dcd20ac74654a8f70bcb6772dc8
Thomas Leitner <t_leitner@gmx.at>
2012-08-08 22:21:38 +0200

    New option for "show config" command to only show modified config options

--- lib/webgen/cli/show_config_command.rb
+++ lib/webgen/cli/show_config_command.rb
@@ -17,21 +17,32 @@ well as the currently used value are displayed.
 
 If an argument is given, only those options that have the argument in their name
 are displayed.
+
+Hint: A debug message will appear at the top of the output if there are unknown
+configuration options and the log level is set to debug.
 DESC
         self.options = CmdParse::OptionParserWrapper.new do |opts|
           opts.separator "Options:"
+          opts.on("-m", "--modified",
+                  *Utils.format_option_desc("Show modified configuration options only")) do |v|
+            @modified = true
+          end
           opts.on("-v", "--[no-]verbose",
                   *Utils.format_option_desc("Verbose output (e.g. option descriptions)")) do |v|
             @verbose = v
           end
         end
+        @modified = false
+        @unknown = true
         @verbose = false
       end
 
       def execute(args)
         config = commandparser.website.config
         selector = args.first.to_s
-        config.options.select {|n, d| n.include?(selector)}.sort.each do |name, data|
+        config.options.select do |n, d|
+          n.include?(selector) && (!@modified || config[n] != d.default)
+        end.sort.each do |name, data|
           format_config_option(name, data, config[name])
         end
       end
