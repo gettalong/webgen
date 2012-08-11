54d855880588835d840a94466a9b38729e7f13c4
Thomas Leitner <t_leitner@gmx.at>
2012-08-11 13:06:59 +0200

    Updated content processor Fragments

--- lib/webgen/content_processor/fragments.rb
+++ lib/webgen/content_processor/fragments.rb
@@ -1,26 +1,77 @@
 # -*- encoding: utf-8 -*-
 
 require 'webgen/content_processor'
+require 'webgen/path'
 
 module Webgen
   class ContentProcessor
 
     # Uses the HTML headers h1, h2, ..., h6 to generate nested fragment nodes.
-    class Fragments
+    module Fragments
 
-      include Webgen::WebsiteAccess
-
-      # Generate the nested fragment nodes from <tt>context.content</tt> under
-      # <tt>content.content_node</tt> but only if there is no associated <tt>:block</tt> data in
-      # +context+ or the block is named +content+.
-      def call(context)
-        if !context[:block] || context[:block].name == 'content'
-          sections = website.blackboard.invoke(:parse_html_headers, context.content)
-          website.blackboard.invoke(:create_fragment_nodes, sections, context.content_node,
-                                    website.blackboard.invoke(:source_paths)[context.content_node.node_info[:src]],
-                                    context.content_node.meta_info['fragments_in_menu'])
+      # Create the nested fragment nodes from the content under the content node but only if there
+      # is no associated block or the block is named +content+.
+      def self.call(context)
+        if !context[:block_name] || context[:block_name] == 'content'
+          sections = parse_html_headers(context.content)
+          create_fragment_nodes(context, sections, context.content_node)
         end
         context
+      end
+
+      HTML_HEADER_REGEXP = /<h([123456])(?:>|\s([^>]*)>)(.*?)<\/h\1\s*>/i
+      HTML_ATTR_REGEXP = /\s*(\w+)\s*=\s*('|")(.+?)\2\s*/
+
+      # Parse the string +content+ for headers +h1+, ..., +h6+ and return the found, nested
+      # sections.
+      #
+      # Only those headers are used which have an +id+ attribute set. The method returns a list of
+      # arrays with entries <tt>level, id, title, sub sections</tt> where <tt>sub sections</tt> is
+      # such a list again.
+      def self.parse_html_headers(content)
+        sections = []
+        stack = []
+        content.scan(HTML_HEADER_REGEXP).each do |level,attrs,title|
+          next if attrs.nil?
+          id_attr = attrs.scan(HTML_ATTR_REGEXP).find {|name,sep,value| name == 'id'}
+          next if id_attr.nil?
+          id = id_attr[2]
+
+          section = [level.to_i, id, title, []]
+          success = false
+          while !success
+            if stack.empty?
+              sections << section
+              stack << section
+              success = true
+            elsif stack.last.first < section.first
+              stack.last.last << section
+              stack << section
+              success = true
+            else
+              stack.pop
+            end
+          end
+        end
+        sections
+      end
+
+      # Create nested fragment nodes under +parent+ from +sections+ (which can be created using
+      # #parse_html_headers).
+      #
+      # The meta info +sort_info+ is calculated from the base +si+ value.
+      def self.create_fragment_nodes(context, sections, parent, si = 1000)
+        sections.each do |level, id, title, sub_sections|
+          path = Webgen::Path.new(parent.alcn.sub(/#.*$/, '') + '#' + id)
+          path.meta_info['parent_alcn'] = parent.alcn
+          path.meta_info['pipeline'] = []
+          path.meta_info['no_output'] = true
+          path.meta_info['title'] = title
+          path.meta_info['sort_info'] = si = si.succ
+          node = context.website.ext.path_handler.create_secondary_nodes(path, '', 'copy', context.content_node.alcn).first
+
+          create_fragment_nodes(context, sub_sections, node, si.succ)
+        end
       end
 
     end
---
+++ test/webgen/content_processor/test_fragments.rb
@@ -0,0 +1,96 @@
+# -*- encoding: utf-8 -*-
+
+require 'webgen/test_helper'
+require 'webgen/content_processor/fragments'
+require 'yaml'
+
+class TestContentProcessorFragments < MiniTest::Unit::TestCase
+
+  include Webgen::TestHelper
+
+  TEST_CONTENT=<<EOF
+- data: |
+    <h1 id="test" style="test">Test</h1>
+    <h2 id="other">test</h2>
+    <h1>nothing</h1>
+    <h1 id="third">test</h1>
+  sections:
+    - [1, test, Test]
+    - [2, other, test]
+    - [1, third, test]
+
+- data: |
+    <h1 id="test">Test</h1>
+    <h2 id="other">test</h2>
+    <h3 id='non'>other</h3>
+    <h1 id="third">test</h1>
+    <h3 id='four'>fourth</h3>
+  sections:
+    - [1, test, Test]
+    - [2, other, test]
+    - [3, non, other]
+    - [1, third, test]
+    - [3, four, fourth]
+EOF
+
+  def setup
+    @cp = Webgen::ContentProcessor::Fragments
+  end
+
+  def test_static_parse_html_headers
+    YAML::load(TEST_CONTENT).each do |data|
+      sections = @cp.parse_html_headers(data['data'])
+      check_sections(sections, data['sections'])
+    end
+  end
+
+  def check_sections(sections, valid)
+    sections.each do |level, id, title, subsecs|
+      assert_equal(valid.shift, [level, id, title])
+      check_sections(subsecs, valid)
+    end
+  end
+
+  def test_static_call
+    setup_website
+    @website.ext.path_handler = Object.new
+    def (@website.ext.path_handler).create_secondary_nodes(path, body, handler, source_node)
+      @proc.call(path, body, handler, source_node)
+    end
+    create_secondary_nodes = lambda do |path, body, handler, source_node|
+      assert_equal('', body)
+      assert_equal('copy', handler)
+      assert_equal(@website.tree['/test.html'].alcn, source_node)
+      parent = @website.tree[path.meta_info['parent_alcn']]
+      [Webgen::Node.new(parent, path.cn, path.alcn, path.meta_info)]
+    end
+    @website.ext.path_handler.instance_variable_set(:@proc, create_secondary_nodes)
+
+    root = Webgen::Node.new(@website.tree.dummy_root, '/', '/')
+    node = Webgen::Node.new(root, 'test.html', '/test.html')
+    node.node_info[:src] = '/test.html'
+
+    context = Webgen::Context.new(@website, :chain => [node])
+    context.content = YAML::load(TEST_CONTENT).first['data']
+    @cp.call(context)
+    refute_nil(root.tree['/test.html#test'])
+    refute_nil(root.tree['/test.html#other'])
+    refute_nil(root.tree['/test.html#third'])
+    assert_equal(node, node.resolve('#test').parent)
+    assert_equal(node.resolve('#test'), node.resolve('#other').parent)
+
+    assert_equal(6, root.tree.node_access[:alcn].length)
+    root.tree.delete_node('/test.html#test')
+
+    context[:block_name] = 'content'
+    context.content = '<h1 id="mytest">Test</h1>'
+    @cp.call(context)
+    refute_nil(root.tree['/test.html#mytest'])
+    root.tree.delete_node(root.tree['/test.html#mytest'])
+
+    context[:block_name] = 'other'
+    @cp.call(context)
+    assert_nil(root.tree['/test.html#mytest'])
+  end
+
+end
