a20b612e4c7dc4ce599c6eeb92fe24cd2f69f78f
Thomas Leitner <t_leitner@gmx.at>
2012-10-14 10:10:49 +0200

    Only retrieving item data in ItemTracker if it has not been retrieved before

--- lib/webgen/item_tracker.rb
+++ lib/webgen/item_tracker.rb
@@ -169,7 +169,7 @@ module Webgen
     def add(node, name, *item)
       uid = unique_id(name, item)
       @node_dependencies[node.alcn] << uid
-      @item_data[uid] = item_tracker(name).item_data(*uid.last)
+      @item_data[uid] ||= item_tracker(name).item_data(*uid.last)
     end
 
     # Return +true+ if the given node has changed.
