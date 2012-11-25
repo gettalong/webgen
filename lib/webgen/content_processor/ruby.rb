6f257a69d1f9c795b60cc58b4cccb9e3f3332f99
Thomas Leitner <t_leitner@gmx.at>
2012-11-25 09:10:18 +0100

    ERB::Util methods are now available when using content processor ruby

--- lib/webgen/content_processor/ruby.rb
+++ lib/webgen/content_processor/ruby.rb
@@ -1,12 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 require 'webgen/content_processor'
+require 'erb'
 
 module Webgen
   class ContentProcessor
 
     # Processes the content that is valid Ruby to generate new content.
     module Ruby
+
+      extend ERB::Util
 
       # Process the content of +context+ which needs to be valid Ruby code.
       def self.call(context)
