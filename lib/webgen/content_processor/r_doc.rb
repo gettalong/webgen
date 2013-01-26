13a99ae180af89064e6f0f9e5dcdca0b2ed2da95
Thomas Leitner <t_leitner@gmx.at>
2013-01-26 18:24:08 +0100

    The rdoc content processor now needs at least version 4.0.0 of rdoc

--- lib/webgen/content_processor/r_doc.rb
+++ lib/webgen/content_processor/r_doc.rb
@@ -1,18 +1,31 @@
 # -*- encoding: utf-8 -*-
 
 require 'webgen/content_processor'
-webgen_require 'rdoc/markup/to_html', 'rdoc'
+
+### Hack...
+# Workaround to load the correct version of rdoc. Unusable versions don't have a rdoc/store file and
+# so we can use this to our advantage.
+begin
+  # If a useable version is available, this won't fail with a LoadError (but probably a NameError)
+  # and will activate the correct version, even if installed via Rubygems.
+  require 'rdoc/store'
+rescue LoadError
+  webgen_require('rdoc/rdoc', 'rdoc')
+rescue Exception
+end
+require 'rdoc/rdoc'
+
 
 module Webgen
   class ContentProcessor
 
     # Converts content in RDoc markup (the native Ruby documentation format) to HTML. Needs the newer
-    # RDoc implementation (version >= 2.4.x).
+    # RDoc implementation (version >= 4.0.0).
     module RDoc
 
       # Convert the content in RDoc markup to HTML.
       def self.call(context)
-        context.content = ::RDoc::Markup::ToHtml.new.convert(context.content)
+        context.content = ::RDoc::Markup::ToHtml.new(::RDoc::Options.new).convert(context.content)
         context
       end
 
